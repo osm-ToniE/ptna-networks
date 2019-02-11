22263e22e6f4c313181a01e252115ab1081d8e96
ToniE <osm-ToniE@web.de>
2019-02-11 11:34:52 +0100

    Change name of 'network'
    Landshuter Stadtbusnetz and Landshuter Regionalbusnetz merged to Landshuter Verkehrsverbund (LAVV) on 1.1.2019
    For a certain period of time, all three are valid

--- DE-BY-LA/settings.sh
+++ DE-BY-LA/settings.sh
@@ -4,14 +4,14 @@
 # set variales for analysis of network
 #
 
-PREFIX="DE-BY-LA"
+PREFIX="DE-BY-LAVV"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name~'Landshut'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
-NETWORK_LONG="Landshuter Stadtbusnetz|Landshuter Regionalbusnetz"
-NETWORK_SHORT="LA"
+NETWORK_LONG="Landshuter Verkehrsverbund|Landshuter Stadtbusnetz|Landshuter Regionalbusnetz"
+NETWORK_SHORT="LAVV|LA"
 
 ANALYSIS_PAGE="Landshut/Transportation/Analyse"
-WIKI_ROUTES_PAGE="Landshut/Transportation/Analyse/DE-BY-LA-Linien"
+WIKI_ROUTES_PAGE="Landshut/Transportation/Analyse/DE-BY-LAVV-Linien"
 
 ANALYSIS_OPTIONS="--language=de --check-access --check-name --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=allow --positive-notes --coloured-sketchline"
 
