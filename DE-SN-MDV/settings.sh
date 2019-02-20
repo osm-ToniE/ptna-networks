4c9af92c654f5c2661a64b7c2afe23283479efdb
Toni Erdmann <Toni.Erdmann@web.de>
2019-02-21 00:31:57 +0100

    change Overpass-API query string

--- DE-SN-MDV/settings.sh
+++ DE-SN-MDV/settings.sh
@@ -6,7 +6,7 @@
 
 PREFIX="DE-SN-MDV"
 
-OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name~'(Leipzig|Nordsachsen|Halle .Saale.|Saalekreis|Burgenlandkreis|Altenburger Land)'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=public_transport][name='Mitteldeutscher Verkehrsverbund GmbH'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
 NETWORK_LONG="Mitteldeutscher Verkehrsverbund"
 NETWORK_SHORT="MDV"
 
@@ -22,6 +22,5 @@ ANALYSIS_OPTIONS="--language=de --max-error=10 --positive-notes --check-access -
 # --expect-network-long-for=
 # --relaxed-begin-end-for=
 
-
-# [http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname~%27(Leipzig%7CNordsachsen%7CHalle%20.Saale.%7CSaalekreis%7CBurgenlandkreis%7CAltenburger%20Land)%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B Auf der OSM-Karte anzeigen].
+# http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(%20relation%5Bboundary%3Dpublic_transport%5D%5Bname%3D%27Mitteldeutscher%20Verkehrsverbund%20GmbH%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B
 
