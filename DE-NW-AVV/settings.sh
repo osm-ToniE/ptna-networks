e63ab3e0d81023e823deff34829756a7c4b2e0ac
Toni Erdmann <Toni.Erdmann@web.de>
2019-03-12 21:40:59 +0100

    add new 'network' Aachener Verkehrsverbund

---
+++ DE-NW-AVV/settings.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# set variales for analysis of network
+#
+
+PREFIX="DE-NW-AVV"
+
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=public_transport][name='Aachener Verkehrsverbund'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);<<;rel[type='route'](r);)->.routes;(.routes;rel(r.routes);way(r.routes);node(r.routes););out;"
+NETWORK_LONG="Aachener Verkehrsverbund"
+NETWORK_SHORT="AVV"
+
+#EXPECT_NETWORK_SHORT_FOR="Aachener Verkehrsverbund"
+#EXPECT_NETWORK_SHORT_AS="AVV"
+
+ANALYSIS_PAGE="DE:Aachener_Verkehrsverbund/Analyse"
+WIKI_ROUTES_PAGE="DE:Aachener_Verkehrsverbund/Analyse/AVV-Linien"
+
+ANALYSIS_OPTIONS="--language=de --positive-notes --max-error=10 --check-access --check-name-relaxed --check-sequence --check-osm-separator --check-motorway-link --check-stop-position --multiple-ref-type-entries=analyze --relaxed-begin-end-for=train,light_rail,tram,bus --coloured-sketchline"
+
+# --expect-network-long
+# --check-bus-stop 
+# --expect-network-short
+
