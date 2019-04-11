d9d8be7db7b799effbcd935ff002ec40b1c93942
Toni Erdmann <Toni.Erdmann@web.de>
2019-04-11 20:08:55 +0200

    set language=fr; prepare for re-using Overpass XML
    although not yet fully supported: set --language=fr
    
    define OVERPASS_RESUSE_ID
      preparation for more code in "ptna-network.sh"
      reuse downloaded Overpass XML data from other 'network' if the
      data exists already and is not older than 1 hour (for instance)

--- FR-IDF-arlequin/settings.sh
+++ FR-IDF-arlequin/settings.sh
@@ -7,6 +7,9 @@
 
 PREFIX="FR-IDF-arlequin"
 
+# avoid downloading same area/data if the data has already been downloaded and is not older than 1 hour (start analysis with: "ptna-networks.sh -fo" to 'f'orce download)
+OVERPASS_REUSE_ID="FR-IDF-Q13917"
+
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata=Q13917][type=boundary];(rel(area)[route~'bus'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
 NETWORK_LONG=""
 NETWORK_SHORT="Arlequin"
@@ -18,7 +21,7 @@ ANALYSIS_TALK=""
 #
 WIKI_ROUTES_PAGE=""
 
-ANALYSIS_OPTIONS="--check-access --check-bus-stop --check-name --check-osm-separator --check-sequence --check-stop-position --check-version --coloured-sketchline --check-motorway-link --max-error=10 --multiple-ref-type-entries=analyze --positive-notes"
+ANALYSIS_OPTIONS="--language=fr --check-access --check-bus-stop --check-name --check-osm-separator --check-sequence --check-stop-position --check-version --coloured-sketchline --check-motorway-link --max-error=10 --multiple-ref-type-entries=analyze --positive-notes"
 
 # --language=en
 # --check-bus-stop 
