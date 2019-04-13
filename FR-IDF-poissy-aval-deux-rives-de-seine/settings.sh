e08ef4ae82cba6251ba74506f75127f708486169
Noémie <noemie.lehuby@zaclys.net>
2019-04-13 12:40:48 +0200

    FR IDF - fix some values for network

--- FR-IDF-poissy-aval-deux-rives-de-seine/settings.sh
+++ FR-IDF-poissy-aval-deux-rives-de-seine/settings.sh
@@ -11,8 +11,8 @@ PREFIX="FR-IDF-poissy-aval-deux-rives-de-seine"
 OVERPASS_REUSE_ID="FR-IDF-Q13917"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata=Q13917][type=boundary];(rel(area)[route~'bus'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
-NETWORK_LONG=""
-NETWORK_SHORT="Poissy Aval - Deux Rives de Seine"
+NETWORK_LONG="Poissy Aval - Deux Rives de Seine"
+NETWORK_SHORT="Poissy Aval - 2 Rives de Seine"
 
 ANALYSIS_PAGE=""
 ANALYSIS_TALK=""
@@ -24,11 +24,9 @@ WIKI_ROUTES_PAGE=""
 ANALYSIS_OPTIONS="--language=fr --check-access --check-bus-stop --check-name --check-osm-separator --check-sequence --check-stop-position --check-version --coloured-sketchline --check-motorway-link --max-error=10 --multiple-ref-type-entries=analyze --positive-notes"
 
 # --language=en
-# --check-bus-stop 
+# --check-bus-stop
 # --expect-network-short
 # --expect-network-long
 # --expect-network-short-for=
 # --expect-network-long-for=
 # --relaxed-begin-end-for=
-
-    
