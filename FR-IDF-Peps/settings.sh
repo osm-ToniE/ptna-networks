86693ae5f25ff6d7919acf7dbf7cfce773aac25f
Toni Erdmann <Toni.Erdmann@web.de>
2019-04-23 20:50:39 +0200

    set --check-name-relaxed; improve OVERPASS_REUSE_ID

--- FR-IDF-Peps/settings.sh
+++ FR-IDF-Peps/settings.sh
@@ -7,7 +7,7 @@
 PREFIX="FR-IDF-Peps"
 
 # avoid downloading same area/data if the data has already been downloaded and is not older than 1 hour (start analysis with: "ptna-networks.sh -fo" to 'f'orce download)
-OVERPASS_REUSE_ID="FR-IDF-Q12753"
+OVERPASS_REUSE_ID="FR-IDF-Q12753-bus"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata=Q12753][type=boundary];(rel(area)[route~'bus'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
 NETWORK_LONG=""
@@ -20,7 +20,7 @@ ANALYSIS_TALK="Talk:WikiProject_France/Bus_Pep's/Analysis"
 #
 WIKI_ROUTES_PAGE=""
 
-ANALYSIS_OPTIONS="--language=fr --check-access --check-bus-stop --check-name --check-osm-separator --check-sequence --check-stop-position --check-version --coloured-sketchline --check-motorway-link --max-error=10 --multiple-ref-type-entries=analyze --positive-notes"
+ANALYSIS_OPTIONS="--language=fr --check-access --check-bus-stop --check-name-relaxed --check-osm-separator --check-sequence --check-stop-position --check-version --coloured-sketchline --check-motorway-link --max-error=10 --multiple-ref-type-entries=analyze --positive-notes"
 
 # --check-bus-stop 
 # --expect-network-short
--- FR-IDF-pays-de-l-ourcq/settings.sh
+++ FR-IDF-pays-de-l-ourcq/settings.sh
@@ -8,7 +8,7 @@
 PREFIX="FR-IDF-pays-de-l-ourcq"
 
 # avoid downloading same area/data if the data has already been downloaded and is not older than 1 hour (start analysis with: "ptna-networks.sh -fo" to 'f'orce download)
-OVERPASS_REUSE_ID="FR-IDF-Q13917"
+OVERPASS_REUSE_ID="FR-IDF-Q13917-bus"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata=Q13917][type=boundary];(rel(area)[route~'bus'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
 NETWORK_LONG=""
@@ -21,7 +21,7 @@ ANALYSIS_TALK=""
 #
 WIKI_ROUTES_PAGE=""
 
-ANALYSIS_OPTIONS="--language=fr --check-access --check-bus-stop --check-name --check-osm-separator --check-sequence --check-stop-position --check-version --coloured-sketchline --check-motorway-link --max-error=10 --multiple-ref-type-entries=analyze --positive-notes"
+ANALYSIS_OPTIONS="--language=fr --check-access --check-bus-stop --check-name-relaxed --check-osm-separator --check-sequence --check-stop-position --check-version --coloured-sketchline --check-motorway-link --max-error=10 --multiple-ref-type-entries=analyze --positive-notes"
 
 # --language=en
 # --check-bus-stop 
