a45c0613d2407fa94fea3a6b4cab4a2eb2900d3f
Toni Erdmann <Toni.Erdmann@web.de>
2019-07-10 23:23:50 +0200

    allow 'Kreisverkehr' with small 'v'

--- DE-BW-KV.SHA/settings.sh
+++ DE-BW-KV.SHA/settings.sh
@@ -7,7 +7,7 @@
 PREFIX="DE-BW-KV.SHA"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name~'Landkreis Schwäbisch Hall'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
-NETWORK_LONG="KreisVerkehr Schwäbisch Hall"
+NETWORK_LONG="KreisVerkehr Schwäbisch Hall|KreisVerkehr Schwäbisch Hall"
 NETWORK_SHORT="KV SHA"
 
 ANALYSIS_PAGE="Landkreis_Schwäbisch_Hall/KV.SHA/Analyse"
