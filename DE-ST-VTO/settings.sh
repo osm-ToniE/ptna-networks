0ec7d3a97b6581421006b2e67a2c683edbd144d3
Toni Erdmann <Toni.Erdmann@web.de>
2019-07-12 01:14:09 +0200

    add settings dfor new 'network' DE-ST-VTO

---
+++ DE-ST-VTO/settings.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# set variales for analysis of network
+#
+
+PREFIX="DE-ST-VTO"
+
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name='Landkreis Harz'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
+NETWORK_LONG="Verkehrs- und Tarifgemeinschaft Ostharz|Harzer Verkehrsbetriebe|Harzer Schmalspurbahnen"
+NETWORK_SHORT="VTO|HVB|HSB"
+
+ANALYSIS_PAGE="Landkreis_Harz/Transportation/Analyse"
+ANALYSIS_TALK="Talk:Landkreis_Harz/Transportation/Analyse"
+WIKI_ROUTES_PAGE="Landkreis_Harz/Transportation/Analyse/DE-ST-VTO-Linien"
+
+ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
+
+# --check-bus-stop 
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+# --relaxed-begin-end-for=
+
