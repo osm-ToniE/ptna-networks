26d7578680be30525bfe49a7cd386fb280e9d7aa
Toni Erdmann <Toni.Erdmann@web.de>
2019-08-03 15:35:34 +0200

    new 'network' VVM in DE-BY Mainfranken

---
+++ DE-BY-VVM-Mainfranken/settings.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# set variales for analysis of network
+#
+
+PREFIX="DE-BY-VVM-Mainfranken"
+
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name~'(Würzburg|Landkreis Main-Spessart|Landkreis Kitzingen)'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(r.routes););out;"
+NETWORK_LONG="Verkehrsunternehmens-Verbund Mainfranken"
+NETWORK_SHORT="VVM"
+
+ANALYSIS_PAGE="Mainfranken/Transportation/Analyse"
+WIKI_ROUTES_PAGE="Mainfranken/Transportation/VVM-Linien-gesamt"
+
+ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=allow --positive-notes --coloured-sketchline"
+
+# --check-bus-stop 
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+# --relaxed-begin-end-for=
