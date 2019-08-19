6ef2f4addcbd61572543ee47ccbfcd26dc2628ae
ToniE <osm-ToniE@web.de>
2019-08-19 12:30:46 +0200

    change settings
    check names relaxed and allow PTv1 compatibility

--- DE-BY-VGN/settings.sh
+++ DE-BY-VGN/settings.sh
@@ -14,7 +14,7 @@ ANALYSIS_PAGE="Nürnberg/Transportation/Analyse"
 ANALYSIS_TALK="Talk:Nürnberg/Transportation/Analyse"
 WIKI_ROUTES_PAGE="Nürnberg/Transportation/Analyse/DE-BY-VGN-Linien"
 
-ANALYSIS_OPTIONS="--language=de --max-error=10 --check-sequence --check-access --check-name --check-stop-position --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
+ANALYSIS_OPTIONS="--language=de --max-error=10 --check-sequence --check-access --check-name-relaxed --check-stop-position --check-version --check-osm-separator --check-motorway-link --ptv1-compatibility=allow --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
 
 # --check-bus-stop 
 # --expect-network-long
