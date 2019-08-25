945c02c60656e8da60dc87edf39df50de4aa133e
Toni Erdmann <Toni.Erdmann@web.de>
2019-08-25 18:19:18 +0200

    shift TALK-Page

--- DE-NI-VRB/settings.sh
+++ DE-NI-VRB/settings.sh
@@ -10,7 +10,8 @@ OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=public
 NETWORK_LONG="Verkehrsverbund Region Braunschweig|Verbundtarif Region Braunschweig|Verbundtarif Region Braunschweig (VRB)"
 NETWORK_SHORT="VRB"
 
-ANALYSIS_TALK="Verkehrsverbund_Region_Braunschweig/Analyse"
+ANALYSIS_PAGE="Verkehrsverbund_Region_Braunschweig/Analyse"
+ANALYSIS_TALK="Talk:Verkehrsverbund_Region_Braunschweig/Analyse"
 WIKI_ROUTES_PAGE="Verkehrsverbund_Region_Braunschweig/Analyse/DE-NI-VRB-Routes"
 
 ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name-relaxed --check-route-ref --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --relaxed-begin-end-for=train,light_rail,monorail,tram"
