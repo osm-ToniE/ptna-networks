c1b1c7c7de53641706686dd515b3ec1faaa10128
Toni Erdmann <Toni.Erdmann@web.de>
2019-10-28 19:31:23 +0100

    add new 'network' - US Flixbus

---
+++ UTC-07/US/US-Flixbus/settings.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# set variales for analysis of network
+#
+
+PREFIX="US-Flixbus"
+
+PTNA_TIMEZONE="America/Los_Angeles"
+
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=[timeout:1800];area[wikidata=%27Q30%27];(rel(area)[route~%27(bus|coach)%27][~%27network|operator%27~%27(Fernb|fernb|Flix|flix)%27];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Flixbus"
+NETWORK_SHORT=""
+
+ANALYSIS_PAGE= #"Europa/Transportation/Analyse"
+ANALYSIS_TALK= #"Talk:Europa/Transportation/Analyse"
+WIKI_ROUTES_PAGE= #"Europa/Transportation/Analyse/Flixbuslinien"
+
+ANALYSIS_OPTIONS="--allow-coach --check-access --check-name --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
+
+# --check-bus-stop 
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+# 
+
+
