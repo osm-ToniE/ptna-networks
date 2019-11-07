7ca51dae9e93f343e7a8409f2f4854ab2347eb0b
Toni Erdmann <Toni.Erdmann@web.de>
2019-11-07 22:09:08 +0100

    entend settings for issue #3

--- UTC+01/DE/SL/DE-SL-saarVV/settings.sh
+++ UTC+01/DE/SL/DE-SL-saarVV/settings.sh
@@ -21,3 +21,31 @@ ANALYSIS_OPTIONS="--language=de --check-access --check-name-relaxed --check-stop
 # --expect-network-short-for=
 # --expect-network-long-for=
 
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# column 1 = Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# column 2 = Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME=" 	Saarland"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3D%27public_transport%27%5D%5Bname%3D%22Saarl%C3%A4ndischer%20Verkehrsverbund%22%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+
+# column 3 = Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Saarländischer Verkehrsverbund"
+PTNA_WWW_NETWORK_LINK=""
+
+# column 4 = Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# column 5 = Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# column 6 = Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Diskussion"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# column 7 Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="saarVV Linien"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
+
