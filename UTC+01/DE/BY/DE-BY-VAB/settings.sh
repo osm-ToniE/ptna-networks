c2489ffa71f070df6692903c7c31fa35ad1bf4b8
ToniE <osm-ToniE@web.de>
2019-11-12 17:23:59 +0100

    expect_network_short is OFF

--- UTC+01/DE/BY/DE-BY-VAB/settings.sh
+++ UTC+01/DE/BY/DE-BY-VAB/settings.sh
@@ -14,9 +14,10 @@ NETWORK_SHORT="VAB"
 #ANALYSIS_TALK="Talk:???/Analyse"
 #WIKI_ROUTES_PAGE="???/Analyse/DE-BY-VAB-Linien"
 
-ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --expect-network-short --ptv1-compatibility=allow --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --ptv1-compatibility=allow --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
 # --check-bus-stop 
+# --expect-network-short
 # --expect-network-long
 # --expect-network-short-for=
 # --expect-network-long-for=
