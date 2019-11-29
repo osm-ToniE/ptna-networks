6484e670dc9f9f7d6bc98ac7daa48c23f010a5ed
Toni Erdmann <Toni.Erdmann@web.de>
2019-11-29 20:03:43 +0100

    rename DE-BY-VLP' to 'DE-BY-VBP-VLP' to also include 'network' of the city of Passau

--- UTC+01/DE/BY/DE-BY-VBP-VLP/settings.sh
+++ UTC+01/DE/BY/DE-BY-VBP-VLP/settings.sh
@@ -4,15 +4,15 @@
 # set variales for analysis of network
 #
 
-PREFIX="DE-BY-VLP"
+PREFIX="DE-BY-VBP-VLP"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name='Passau'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Verkehrsgemeinschaft Landkreis Passau|Verkehrsbetriebsgesellschaft Passau"
-NETWORK_SHORT="VLP|VBP"
+NETWORK_LONG="Verkehrsbetriebsgesellschaft Passau|Verkehrsgemeinschaft Landkreis Passau"
+NETWORK_SHORT="VBP|VLP"
 
 ANALYSIS_PAGE="Passau/Transportation/Analyse"
 ANALYSIS_TALK="Talk:Passau/Transportation/Analyse"
-WIKI_ROUTES_PAGE="Passau/Transportation/Analyse/DE-BY-VLP-Linien"
+WIKI_ROUTES_PAGE="Passau/Transportation/Analyse/DE-BY-VBP-VLP-Linien"
 
 ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=allow --positive-notes --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
@@ -33,7 +33,7 @@ PTNA_WWW_REGION_NAME="Stadt und Landkreis Passau"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname~%27Passau%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # column 3 = Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Verkehrsgemeinschaft Landkreis Passau;Verkehrsbetriebsgesellschaft Passau"
+PTNA_WWW_NETWORK_NAME="Verkehrsbetriebsgesellschaft Passau; Verkehrsgemeinschaft Landkreis Passau"
 PTNA_WWW_NETWORK_LINK=""
 
 # column 4 = Date and Time of last analysis in UTC and Local Time format
