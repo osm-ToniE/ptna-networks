5503a9b10fa614e229abba53f4e6a532ea3a737b
ToniE <osm-ToniE@web.de>
2019-12-05 09:42:57 +0100

    allow also long 'network'

--- UTC+01/DE/BW/DE-BW-RVF/settings.sh
+++ UTC+01/DE/BW/DE-BW-RVF/settings.sh
@@ -14,9 +14,10 @@ ANALYSIS_PAGE="Regio-Verkehrsverbund_Freiburg/Analyse"
 ANALYSIS_TALK="Talk:Regio-Verkehrsverbund_Freiburg/Analyse"
 WIKI_ROUTES_PAGE="Regio-Verkehrsverbund_Freiburg/Analyse/DE-BW-RVF-Routes"
 
-ANALYSIS_OPTIONS="--language=de --check-access --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --expect-network-short --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=de --check-access --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
 # --check-bus-stop 
+# --expect-network-short
 # --expect-network-long
 # --expect-network-short-for=
 # --expect-network-long-for=
