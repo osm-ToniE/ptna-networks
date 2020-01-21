92977942249995af77619e832e64ddeb6d001dff
ToniE <osm-ToniE@web.de>
2020-01-21 14:04:44 +0100

    fix typo

--- UTC+01/DE/BW/DE-BW-VHB/settings.sh
+++ UTC+01/DE/BW/DE-BW-VHB/settings.sh
@@ -30,7 +30,7 @@ ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name --che
 
 # column 2 = Name + Link to Overpass-Turbo call to show area on map
 PTNA_WWW_REGION_NAME="Landkreis Konstanz"
-PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname=%27Landkreis%20Konstanz%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname~%27Landkreis%20Konstanz%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # column 3 = Name + Link to the network provider / transport association
 PTNA_WWW_NETWORK_NAME="Verkehrsverbund Hegau-Bodensee"
