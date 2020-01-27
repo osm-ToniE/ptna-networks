841d613f3ec65267f18dcb6962be65aaaf445db6
Toni Erdmann <Toni.Erdmann@web.de>
2020-01-27 22:27:53 +0100

    small fix

--- UTC+01/CH/CH-CTM/settings.sh
+++ UTC+01/CH/CH-CTM/settings.sh
@@ -33,7 +33,7 @@ PTNA_WWW_REGION_NAME="Cantone Ticino (TI) e Distretto di Moësa (GR)"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Bwikidata~%22^(Q12724|Q23013974)$%22%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # column 3 = Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Comunità tariffale Arcobaleno"
+PTNA_WWW_NETWORK_NAME="Comunità tariffale Arcobaleno «Arcobaleno»"
 PTNA_WWW_NETWORK_LINK="https://www.arcobaleno.ch/"
 
 # column 4 = Date and Time of last analysis in UTC and Local Time format
