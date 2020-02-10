fc7a3efbdd9550ebe334a3ea0d1653419eb5e334
ToniE <osm-ToniE@web.de>
2020-02-10 15:45:52 +0100

    add 'network' variants

--- UTC+01/CH/CH-TVOEng/settings.sh
+++ UTC+01/CH/CH-TVOEng/settings.sh
@@ -7,7 +7,7 @@
 PREFIX="CH-TVOEng"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][wikidata~'^(Q23732284)$'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG=" Tarifverbund Oberengadin|Engadin Mobil|engadin mobil|Engadin-Mobil|engadin-mobil"
+NETWORK_LONG=" Tarifverbund Oberengadin|Engadin Mobil|engadin mobil|Engadin-Mobil|engadin-mobil|Engadin Bus|Engadinbus|Engadinmobil"
 NETWORK_SHORT="TVOEng"
 
 ANALYSIS_PAGE="Switzerland:Public_Transport/Engadin-Mobil/Analysis"
