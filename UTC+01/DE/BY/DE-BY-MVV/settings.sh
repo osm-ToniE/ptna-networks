a9dc91772b30c33170da09655ae438b3ec4ab152
ToniE <osm-ToniE@web.de>
2020-02-26 14:13:59 +0100

    extend list of 'network' to be analyzed

--- UTC+01/DE/BY/DE-BY-MVV/settings.sh
+++ UTC+01/DE/BY/DE-BY-MVV/settings.sh
@@ -7,7 +7,7 @@
 PREFIX="DE-BY-MVV"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=[timeout:300];area[boundary=administrative][admin_level=6][name~'(Dachau|München|Ebersberg|Erding|Starnberg|Freising|Tölz|Wolfratshausen|Fürstenfeldbruck)'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Münchner Verkehrs- und Tarifverbund|Münchner Linien|Linientaxi Grünwald|Gauting|Oberbiberg|Freisinger Parkhaus und Verkehrs-GmbH|Ortsverkehr Markt Glonn"
+NETWORK_LONG="Münchner Verkehrs- und Tarifverbund|Münchner Linien|Linientaxi Grünwald|Bürgerbus Gauting|Bürgerbus Oberbiberg|Stadtbus Dorfen|Freisinger Parkhaus und Verkehrs-GmbH|Ortsverkehr Markt Glonn"
 NETWORK_SHORT="MVV"
 
 ANALYSIS_PAGE="München/Transportation/Analyse"
