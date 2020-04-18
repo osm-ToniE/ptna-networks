4df4e19938ee006f5874f91d3e93bff8a5d72f21
Toni Erdmann <Toni.Erdmann@web.de>
2020-04-18 17:26:14 +0200

    move VRN from BW back to RP

--- UTC+01/DE/RP/DE-RP-VRN/settings.sh
+++ UTC+01/DE/RP/DE-RP-VRN/settings.sh
@@ -4,7 +4,7 @@
 # set variables for analysis of network
 #
 
-PREFIX="DE-BW-VRN"
+PREFIX="DE-RP-VRN"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=public_transport][name='Verkehrsverbund Rhein-Neckar'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Verkehrsverbund Rhein-Neckar"
@@ -12,7 +12,7 @@ NETWORK_SHORT="VRN"
 
 ANALYSIS_PAGE="Verkehrsverbund_Rhein-Neckar/Analyse"
 ANALYSIS_TALK="Talk:Verkehrsverbund_Rhein-Neckar/Analyse"
-WIKI_ROUTES_PAGE="Verkehrsverbund_Rhein-Neckar/Analyse/DE-BW-VRN-Linien"
+WIKI_ROUTES_PAGE="Verkehrsverbund_Rhein-Neckar/Analyse/DE-RP-VRN-Linien"
 
 ANALYSIS_OPTIONS="--language=de --max-error=10 --check-version --check-access --check-stop-position --check-sequence --check-osm-separator --check-osm-separator --check-motorway-link --positive-notes --check-name-relaxed --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
