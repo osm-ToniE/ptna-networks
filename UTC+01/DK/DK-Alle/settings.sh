6a3f63e416ae939423f91689e0e65d2f168686c1
Toni Erdmann <Toni.Erdmann@web.de>
2020-04-23 21:49:35 +0200

    add support for Denmark

---
+++ UTC+01/DK/DK-Alle/settings.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="DK-Alle"
+
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=[timeout:900];area[boundary=administrative][admin_level=2][name='Danmark'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r.routes);node(w);node(r.routes););out;"
+#NETWORK_LONG=""
+#NETWORK_SHORT=""
+NETWORK_LONG=""
+NETWORK_SHORT=""
+
+ANALYSIS_PAGE=""
+ANALYSIS_TALK="" #Talk:"
+WIKI_ROUTES_PAGE=""
+
+ANALYSIS_OPTIONS="--language=dk --check-access --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --positive-notes --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,light_rail,monorail,tram"
+
+# --check-bus-stop 
+# --expect-network-long
+# --expect-network-short-for=
+# --expect-network-long-for=
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# column 1 = Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# column 2 = Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Danmark"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D2%5D%5Bname~%27Danmark%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+
+# column 3 = Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Offentlig transport i Danmark"
+PTNA_WWW_NETWORK_LINK=""
+
+# column 4 = Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# column 5 = Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# column 6 = Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Diskussion"
+PTNA_WWW_DISCUSSION_LINK="" # https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# column 7 Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="Linjer i Danmark"
+PTNA_WWW_ROUTES_LINK="" # https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
+
