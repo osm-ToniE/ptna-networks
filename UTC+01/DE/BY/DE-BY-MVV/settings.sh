c02c434527ad61fee8dfdb6ce76f0b2af66d8c5e
Toni Erdmann <Toni.Erdmann@web.de>
2020-05-05 20:17:13 +0200

    add option '--check-way-type'

--- UTC+01/DE/BY/DE-BY-MVV/settings.sh
+++ UTC+01/DE/BY/DE-BY-MVV/settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# set variales for analysis of network
+# set variables for analysis of network
 #
 
 PREFIX="DE-BY-MVV"
@@ -14,9 +14,9 @@ ANALYSIS_PAGE="München/Transportation/Analyse"
 ANALYSIS_TALK="Talk:München/Transportation/Analyse"
 WIKI_ROUTES_PAGE="München/Transportation/MVV-Linien-gesamt"
 
-ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-route-ref --multiple-ref-type-entries=analyze --coloured-sketchline --expect-network-long --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-route-ref --check-way-type --multiple-ref-type-entries=analyze --coloured-sketchline --expect-network-long --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
-# --check-bus-stop 
+# --check-bus-stop
 # --positive-notes
 # --expect-network-short
 # --expect-network-short-for=
@@ -25,28 +25,27 @@ ANALYSIS_OPTIONS="--language=de --max-error=10 --check-access --check-name-relax
 #
 # extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
 #
-# column 1 = Name + Link to Analysis Result Page on server
+# Name + Link to Analysis Result Page on server
 # automatically build by PHP script
 
-# column 2 = Name + Link to Overpass-Turbo call to show area on map
+# Name + Link to Overpass-Turbo call to show area on map
 PTNA_WWW_REGION_NAME="Region München"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname~%27(Dachau%7CM%C3%BCnchen%7CEbersberg%7CErding%7CStarnberg%7CFreising%7CT%C3%B6lz%7CWolfratshausen%7CF%C3%BCrstenfeldbruck)%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
-# column 3 = Name + Link to the network provider / transport association
+# Name + Link to the network provider / transport association
 PTNA_WWW_NETWORK_NAME="Münchner Verkehrs- und Tarifverbund und weitere"
 PTNA_WWW_NETWORK_LINK="https://www.mvv-muenchen.de/"
 
-# column 4 = Date and Time of last analysis in UTC and Local Time format
+# Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
 
-# column 5 = Date and Time of latest changes in UTC and Local Time format
+# Date and Time of latest changes in UTC and Local Time format
 # automatically build by PHP script
 
-# column 6 = Name + Link to discussion / documentation page (usually in OSM Wiki)
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
 PTNA_WWW_DISCUSSION_NAME="Diskussion"
 PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
-# column 7 Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
 PTNA_WWW_ROUTES_NAME="MVV Linien"
 PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
-
