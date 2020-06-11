1d3ca0ceefcc121aaae508e1b6c65350cfa2c1ef
Toni Erdmann <Toni.Erdmann@web.de>
2020-06-11 18:09:44 +0200

    fix typo in network name

--- UTC+01/DE/BY/DE-BY-VGND/settings.sh
+++ UTC+01/DE/BY/DE-BY-VGND/settings.sh
@@ -7,7 +7,7 @@
 PREFIX="DE-BY-VGND"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name='Landkreis Neuburg-Schrobenhausen'];(rel(area)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type='route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Verkehrsgmeinschaft Neuburg an der Donau|Ingolstädter Verkehrsgesellschaft mbH|Regionalbus Augsburg|Regionalbus Augsburg GmbH|Seitz|Spangler|Stadtbus Schrobenhausen|Stadtlinie Neuburg|Werksbus MBB|Stadtbus SOB"
+NETWORK_LONG="Verkehrsgemeinschaft Neuburg an der Donau|Ingolstädter Verkehrsgesellschaft mbH|Regionalbus Augsburg|Regionalbus Augsburg GmbH|Seitz|Spangler|Stadtbus Schrobenhausen|Stadtlinie Neuburg|Werksbus MBB|Stadtbus SOB"
 NETWORK_SHORT="VGND|INVG|RBA|SOB"
 
 ANALYSIS_PAGE="Neuburg_an_der_Donau/Transportation/Analyse"
@@ -34,7 +34,7 @@ PTNA_WWW_REGION_NAME="Landkreis Neuburg-Schrobenhausen"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname%3D%27Landkreis%20Neuburg-Schrobenhausen%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Verkehrsgmeinschaft Neuburg an der Donau und weitere"
+PTNA_WWW_NETWORK_NAME="Verkehrsgemeinschaft Neuburg an der Donau und weitere"
 PTNA_WWW_NETWORK_LINK=""
 
 # Date and Time of last analysis in UTC and Local Time format
