3316f4efe9ba1cae0220951ea7704aac05f78152
Toni Erdmann <Toni.Erdmann@web.de>
2020-12-09 09:28:30 +0100

    some more translations

--- UTC+01/RS/00/RS-00-Beograd/settings.sh
+++ UTC+01/RS/00/RS-00-Beograd/settings.sh
@@ -4,15 +4,15 @@
 # set variables for analysis of network
 #
 
-PREFIX="RS-00-Belgrade"
+PREFIX="RS-00-Beograd"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata~'^(Q2074197)$'][type=boundary];(rel(area)[route~%27(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)%27];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG=""
 NETWORK_SHORT="ITS|ITS 1|ITS 2|ITS 1/2|ITS1|ITS2|ИТС|ИТС 2|БГ:Воз"
 
-ANALYSIS_PAGE=""
-ANALYSIS_TALK=""
-WIKI_ROUTES_PAGE=""
+ANALYSIS_PAGE="Beograd/Javni_prevoz/Analiza"
+ANALYSIS_TALK="Talk:Beograd/Javni_prevoz/Analiza"
+WIKI_ROUTES_PAGE="Beograd/Javni_prevoz/Analiza/Beograd_Linije"
 
 ANALYSIS_OPTIONS="--language=sr --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
 
@@ -30,7 +30,7 @@ ANALYSIS_OPTIONS="--language=sr --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Serbia / Belgrade"
+PTNA_WWW_REGION_NAME="Београд / Србија"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^(Q2074197)$'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
@@ -44,9 +44,9 @@ PTNA_WWW_NETWORK_LINK=""
 # automatically build by PHP script
 
 # Name + Link to discussion / documentation page (usually in OSM Wiki)
-PTNA_WWW_DISCUSSION_NAME= #"Discussion"
-PTNA_WWW_DISCUSSION_LINK= #"https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+PTNA_WWW_DISCUSSION_NAME="Дискусија"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME= #"Belgrade Routes"
-PTNA_WWW_ROUTES_LINK= #"https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
+PTNA_WWW_ROUTES_NAME="Београдске линије"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
