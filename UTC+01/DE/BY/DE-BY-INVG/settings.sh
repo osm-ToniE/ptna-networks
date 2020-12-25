4bcf998638c7a7f2f8892987f2c44b20b129d70e
Toni Erdmann <Toni.Erdmann@web.de>
2020-12-25 13:16:58 +0100

    change analysis options as requested

--- UTC+01/DE/BY/DE-BY-INVG/settings.sh
+++ UTC+01/DE/BY/DE-BY-INVG/settings.sh
@@ -14,13 +14,13 @@ ANALYSIS_PAGE="Ingolstadt/Transportation/Analyse"
 ANALYSIS_TALK="Talk:Ingolstadt/Transportation/Analyse"
 WIKI_ROUTES_PAGE="Ingolstadt/Transportation/INVG-Linien-gesamt"
 
-ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=de --check-bus-stop --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --expect-network-long --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
-# --check-bus-stop
-# --expect-network-long
+# --check-gtfs
 # --expect-network-short
 # --expect-network-short-for=
 # --expect-network-long-for=
+# --positive-notes
 
 #
 # extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
