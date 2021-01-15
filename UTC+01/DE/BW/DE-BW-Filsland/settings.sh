882327a6626229981cd4849a583613d37dd4f998
Toni Erdmann <Toni.Erdmann@web.de>
2021-01-15 10:38:29 +0100

    'Filsland' is integrated into 'VVS' since 2021-01-01

--- UTC+01/DE/BW/DE-BW-Filsland/settings.sh
+++ UTC+01/DE/BW/DE-BW-Filsland/settings.sh
@@ -33,8 +33,8 @@ PTNA_WWW_REGION_NAME="Landkreis Göppingen"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Bwikidata~'^(Q8525)$'%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Filsland Mobilitätsverbund"
-PTNA_WWW_NETWORK_LINK="https://www.filsland.de/"
+PTNA_WWW_NETWORK_NAME="Seit 1.1.2021 im VVS integriert (diese Auswertung wird in Kürze verschwinden)"
+PTNA_WWW_NETWORK_LINK=""
 
 # Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
