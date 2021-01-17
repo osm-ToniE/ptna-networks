a81cdb161748735b65acb5811f8ac0cf76fc7e7d
Toni Erdmann <Toni.Erdmann@web.de>
2021-01-17 09:56:18 +0100

    small modifications on two networks

--- UTC+01/FR/PAC/FR-PAC-Vaucluse/settings.sh
+++ UTC+01/FR/PAC/FR-PAC-Vaucluse/settings.sh
@@ -37,8 +37,8 @@ PTNA_WWW_REGION_NAME="Vaucluse et Villeneuve-lès-Avignon"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^(Q12792|Q1458257)$'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="$NETWORK_LONG"
-PTNA_WWW_NETWORK_LINK="http://www.transcove.com"
+PTNA_WWW_NETWORK_NAME="TER Provence - Alpes - Côte d'Azur;TUB Bollène;Trans'CoVe;TCVO;Sorg'en bus;C mon bus"
+PTNA_WWW_NETWORK_LINK=";;http://www.transcove.com;;;"
 
 # Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
@@ -51,5 +51,5 @@ PTNA_WWW_DISCUSSION_NAME="Discussion"
 PTNA_WWW_DISCUSSION_LINK=""
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME="Lignes $NETWORK_LONG"
+PTNA_WWW_ROUTES_NAME="Lignes Vaucluse"
 PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
