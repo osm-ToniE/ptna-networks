f9d68a5abd0fb5e09d31e29b7af0ff86b2241a92
Toni Erdmann <Toni.Erdmann@web.de>
2021-01-19 00:09:18 +0100

    update, add more information

--- UTC+01/HR/21/HR-21-ZET/settings.sh
+++ UTC+01/HR/21/HR-21-ZET/settings.sh
@@ -6,13 +6,13 @@
 
 PREFIX="HR-21-ZET"
 
-OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata~'^(Q1435)$'][name='Grad Zagreb'][type=boundary];(rel(area)[route~%27(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)%27];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata~'^(Q1435|Q27038)$'][type=boundary];(rel(area)[route~%27(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)%27];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Zagrebački električni tramvaj"
 NETWORK_SHORT="ZET"
 
-ANALYSIS_PAGE=      #"Croatia/Javni_prijevoz/Zagreb/Analiza"
-ANALYSIS_TALK=      #"Talk:Croatia/Javni_prijevoz/Zagreb/Analiza"
-WIKI_ROUTES_PAGE=   #"Croatia/Javni_prijevoz/Zagreb/ZET_Linije"
+ANALYSIS_PAGE="Croatia/Javni_prijevoz/Zagreb/Analiza"
+ANALYSIS_TALK="Talk:Croatia/Javni_prijevoz/Zagreb/Analiza"
+WIKI_ROUTES_PAGE="Croatia/Javni_prijevoz/Zagreb/ZET_Linije"
 
 ANALYSIS_OPTIONS="--language=hr --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
 
@@ -31,7 +31,7 @@ ANALYSIS_OPTIONS="--language=hr --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 
 # Name + Link to Overpass-Turbo call to show area on map
 PTNA_WWW_REGION_NAME="Grad Zagreb"
-PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^(Q1435)$'][name='Grad Zagreb'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^(Q1435|Q27038)$'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
 PTNA_WWW_NETWORK_NAME="Zagrebački električni tramvaj (ZET)"
@@ -45,8 +45,8 @@ PTNA_WWW_NETWORK_LINK="https://www.zet.hr/"
 
 # Name + Link to discussion / documentation page (usually in OSM Wiki)
 PTNA_WWW_DISCUSSION_NAME="Rasprava"
-PTNA_WWW_DISCUSSION_LINK= #"https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
 PTNA_WWW_ROUTES_NAME="ZET Linije"
-PTNA_WWW_ROUTES_LINK= #"https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
