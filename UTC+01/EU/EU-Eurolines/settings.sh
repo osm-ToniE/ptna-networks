aced4181dff93f7befe0bce9975eb94599dee648
Toni Erdmann <Toni.Erdmann@web.de>
2021-03-08 17:18:50 +0100

    Eurolines does not yet have a 'lines' page in OSM-Wiki

--- UTC+01/EU/EU-Eurolines/settings.sh
+++ UTC+01/EU/EU-Eurolines/settings.sh
@@ -49,4 +49,4 @@ PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
 PTNA_WWW_ROUTES_NAME="Eurolines Lines"
-PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
+PTNA_WWW_ROUTES_LINK= #"https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
