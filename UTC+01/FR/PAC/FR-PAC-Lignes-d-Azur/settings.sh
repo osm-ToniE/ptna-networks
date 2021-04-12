b1005661af50e31b497dc983f2206646d6e9ee5c
Toni Erdmann <Toni.Erdmann@web.de>
2021-04-12 19:02:26 +0200

    set link to discussion page

--- UTC+01/FR/PAC/FR-PAC-Lignes-d-Azur/settings.sh
+++ UTC+01/FR/PAC/FR-PAC-Lignes-d-Azur/settings.sh
@@ -50,7 +50,7 @@ PTNA_WWW_NETWORK_LINK="https://www.ter.sncf.com/sud-provence-alpes-cote-d-azur;h
 
 # Name + Link to discussion / documentation page (usually in OSM Wiki)
 PTNA_WWW_DISCUSSION_NAME="Discussion"
-PTNA_WWW_DISCUSSION_LINK=""
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
 PTNA_WWW_ROUTES_NAME="Lignes Métropole Nice Côte d'Azur"
