c6961e54a255d7a411323aee00a6c676832e4766
Toni Erdmann <Toni.Erdmann@web.de>
2021-04-19 17:06:33 +0200

    Eurolines has no CSV-data

--- UTC+01/EU/EU-Eurolines/settings.sh
+++ UTC+01/EU/EU-Eurolines/settings.sh
@@ -14,7 +14,7 @@ NETWORK_SHORT=""
 
 ANALYSIS_PAGE="Europa/Transportation/Analyse/Eurolines"
 ANALYSIS_TALK="Talk:Europa/Transportation/Analyse/Eurolines"
-WIKI_ROUTES_PAGE="Europa/Transportation/Analyse/Eurolines_Linien"
+WIKI_ROUTES_PAGE= # "Europa/Transportation/Analyse/Eurolines_Linien"
 
 ANALYSIS_OPTIONS="--allow-coach --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
 
