5dcb986daff37a9a743b25b41dd4782390514fcc
Toni Erdmann <Toni.Erdmann@web.de>
2021-08-24 12:02:30 +0200

    remove '--expect-network-short' as iD now forces long form

--- UTC+01/DE/HH/DE-HH-HVV/settings.sh
+++ UTC+01/DE/HH/DE-HH-HVV/settings.sh
@@ -16,10 +16,11 @@ ANALYSIS_PAGE="Hamburger_Verkehrsverbund/Analyse"
 ANALYSIS_TALK="Talk:Hamburger_Verkehrsverbund/Analyse"
 WIKI_ROUTES_PAGE="Hamburger_Verkehrsverbund/Analyse/DE-HH-HVV-Routes"
 
-ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --ptv1-compatibility=allow --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --expect-network-short --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --ptv1-compatibility=allow --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
 # --check-bus-stop
 # --expect-network-long
+# --expect-network-short
 # --expect-network-short-for=
 # --expect-network-long-for=
 
