fce219d4b73c443d62f95704e9a55e364d37e0c4
Toni Erdmann <Toni.Erdmann@web.de>
2022-02-08 10:07:01 +0100

    change region_name

--- UTC+05.30/IN/TN/IN-TN-Chennai/settings.sh
+++ UTC+05.30/IN/TN/IN-TN-Chennai/settings.sh
@@ -32,7 +32,7 @@ ANALYSIS_OPTIONS="--check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --che
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Tamil Nadu / Chennai metropolitan area (CMA)"
+PTNA_WWW_REGION_NAME="Chennai metropolitan area / Tamil Nadu"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata='Q955990'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
