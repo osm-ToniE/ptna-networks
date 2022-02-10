372c992acb40735fb74f75c4a26b0bb47749f13c
Toni Erdmann <Toni.Erdmann@web.de>
2022-02-10 19:47:06 +0100

    fix 'network' on island Sylt

--- UTC+01/DE/SH/DE-SH-NAH.SH/settings.sh
+++ UTC+01/DE/SH/DE-SH-NAH.SH/settings.sh
@@ -9,7 +9,7 @@ PREFIX="DE-SH-NAH.SH"
 PTNA_TIMEZONE="Europe/Berlin"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=4][name~'Schleswig-Holstein'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Nahverkehrsverbund Schleswig-Holstein|Sylter Linienverkehr"
+NETWORK_LONG="Nahverkehrsverbund Schleswig-Holstein|Sylter Verkehrsgesellschaft"
 NETWORK_SHORT="NAH.SH|WDR"
 
 ANALYSIS_PAGE="Schleswig-Holstein/NAH.SH/Analyse"
