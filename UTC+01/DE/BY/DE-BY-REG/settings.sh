3025ee8e4d44655f4a6ef9837b3a0a29741a8730
Toni Erdmann <Toni.Erdmann@web.de>
2022-03-31 14:30:52 +0200

    markd these analyzes as to be removed soon

--- UTC+01/DE/BY/DE-BY-REG/settings.sh
+++ UTC+01/DE/BY/DE-BY-REG/settings.sh
@@ -31,8 +31,8 @@ ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Landkreis Regen"
-PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname%3D%27Landkreis%20Regen%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+PTNA_WWW_REGION_NAME="Diese Analyse wird demnächst eingestellt, siehe DE-BY-VDW"
+PTNA_WWW_REGION_LINK=""
 
 # Name + Link to the network provider / transport association
 # Name + Link to the network provider / transport association
