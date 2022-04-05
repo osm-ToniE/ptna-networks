39b0da10ab79e52ec04ea0e9597510b05411bbc2
Toni Erdmann <Toni.Erdmann@web.de>
2022-04-05 14:56:27 +0200

    fix URL

--- UTC+01/DE/BY/DE-BY-VBP-VLP/settings.sh
+++ UTC+01/DE/BY/DE-BY-VBP-VLP/settings.sh
@@ -31,12 +31,12 @@ ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Stadt und Landkreis Passau"
-PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname~%27Passau%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+PTNA_WWW_REGION_NAME="Diese Analyse wird demnächst eingestellt, siehe DE-BY-VBP und DE-BY-VDW"
+PTNA_WWW_REGION_LINK=""
 
 # Name + Link to the network provider / transport association
 PTNA_WWW_NETWORK_NAME="Verkehrsbetriebsgesellschaft Passau;Die Verkehrsgemeinschaft Landkreis Passau ist seit 01.09.2021 Teil des Verbundtarif DonauWald (VDW)"
-PTNA_WWW_NETWORK_LINK="http://www.swp-passau.de/;https://vdw-mobil.de/"
+PTNA_WWW_NETWORK_LINK="https://vbp.stadtwerke-passau.de/;https://vdw-mobil.de/"
 
 # Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
