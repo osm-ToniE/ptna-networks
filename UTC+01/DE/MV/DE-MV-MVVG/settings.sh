accac8f5de7d3fab09fe60409aa1ccade1165d34
Toni Erdmann <Toni.Erdmann@web.de>
2022-04-06 10:53:47 +0200

    add "Müritz-Nationalpark" to 'network'

--- UTC+01/DE/MV/DE-MV-MVVG/settings.sh
+++ UTC+01/DE/MV/DE-MV-MVVG/settings.sh
@@ -9,7 +9,7 @@ PREFIX="DE-MV-MVVG"
 PTNA_TIMEZONE="Europe/Berlin"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name='Mecklenburgische Seenplatte'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Mecklenburg-Vorpommersche-Verkehrsgesellschaft"
+NETWORK_LONG="Mecklenburg-Vorpommersche-Verkehrsgesellschaft|Müritz-Nationalpark"
 NETWORK_SHORT="MVVG"
 
 ANALYSIS_PAGE="Mecklenburg-Vorpommern/ÖPNV/MVVG/Analyse"
