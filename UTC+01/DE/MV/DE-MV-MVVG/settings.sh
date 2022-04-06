10d1db6d40f93d32ad90cadb5d8696987aac23ef
Toni Erdmann <Toni.Erdmann@web.de>
2022-04-06 16:32:55 +0200

    fix typo in network_long

--- UTC+01/DE/MV/DE-MV-MVVG/settings.sh
+++ UTC+01/DE/MV/DE-MV-MVVG/settings.sh
@@ -9,7 +9,7 @@ PREFIX="DE-MV-MVVG"
 PTNA_TIMEZONE="Europe/Berlin"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name='Mecklenburgische Seenplatte'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Gemeinschaftstarif Mecklenburgische Seenplatte|Stadtlinienverkehr Demmin und Waren|Stadtverkehr Neustrelitz|Neubrandenburger Verkehrsbetriebe|Müritz-Nationalpark-Tarif"
+NETWORK_LONG="Gemeinschaftstarif Mecklenburgische Seenplatte|Stadtlinienverkehr Demmin und Waren|Stadtverkehr Neustrelitz|Neubrandenburger Verkehrsbetriebe|Müritz-Nationalpark-Ticket"
 NETWORK_SHORT="GMS|NVB"
 
 ANALYSIS_PAGE="Mecklenburg-Vorpommern/ÖPNV/MVVG/Analyse"
@@ -36,7 +36,7 @@ PTNA_WWW_REGION_NAME="Landkreis Mecklenburgische Seenplatte"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname%3D%27Mecklenburgische%20Seenplatte%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Mecklenburg-Vorpommersche-Verkehrsgesellschaft"
+PTNA_WWW_NETWORK_NAME="Mecklenburg-Vorpommersche Verkehrsgesellschaft"
 PTNA_WWW_NETWORK_LINK="https://mvvg-bus.de/"
 
 # Date and Time of last analysis in UTC and Local Time format
