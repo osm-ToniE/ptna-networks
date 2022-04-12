868e04428f814b53cbee0c5d6b8075aac8705b28
Toni Erdmann <Toni.Erdmann@web.de>
2022-04-12 14:15:58 +0200

    add some more links to 'networks' for "Stadtverkehr"

--- UTC+01/DE/MV/DE-MV-MVVG/settings.sh
+++ UTC+01/DE/MV/DE-MV-MVVG/settings.sh
@@ -36,8 +36,8 @@ PTNA_WWW_REGION_NAME="Landkreis Mecklenburgische Seenplatte"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname%3D%27Mecklenburgische%20Seenplatte%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Mecklenburg-Vorpommersche Verkehrsgesellschaft"
-PTNA_WWW_NETWORK_LINK="https://mvvg-bus.de/"
+PTNA_WWW_NETWORK_NAME="Mecklenburg-Vorpommersche Verkehrsgesellschaft;Stadtverkehr Demmin;Stadtverkehr Neubrandenburg;Stadtverkehr Neustrelitz;Stadtverkehr Waren"
+PTNA_WWW_NETWORK_LINK="https://mvvg-bus.de/;https://mvvg-bus.de/mvvg-stadtverkehr-demmin;https://www.neu-sw.de/busverkehr;https://mvvg-bus.de/mvvg-stadtverkehr-neustrelitz;https://mvvg-bus.de/mvvg-stadtverkehr-waren"
 
 # Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
