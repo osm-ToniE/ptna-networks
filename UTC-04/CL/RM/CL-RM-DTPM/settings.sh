59be6f2654411830df0297627aad6fbbf46648a7
Toni Erdmann <Toni.Erdmann@web.de>
2022-05-12 19:41:07 +0200

    add a new network 'DTPM' in Santiago de Chile

---
+++ UTC-04/CL/RM/CL-RM-DTPM/settings.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="CL-RM-DTPM"
+
+PTNA_TIMEZONE="America/La_Paz"
+
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata~'^Q45632$'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Red Metropolitana de Movilidad"
+NETWORK_SHORT="DTPM"
+
+ANALYSIS_PAGE="Chile/Public_Transport/Analysis"
+ANALYSIS_TALK="Talk:Chile/Public_Transport/Analysis/CL-DTPM-Routes"
+WIKI_ROUTES_PAGE="Chile/Public_Transport/Analysis/CL-DTPM-Routes"
+
+ANALYSIS_OPTIONS="--language=es --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-bus-stop --check-roundabouts --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+
+# --expect-network-short
+# --expect-network-long
+# --expect-network-short-for=
+# --expect-network-long-for=
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Santiago de Chile"
+PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5B%22wikidata%22~%22^Q45632$%22%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Red Metropolitana de Movilidad"
+PTNA_WWW_NETWORK_LINK="https://www.red.cl/"
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Discusiones"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="Red Lines"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
