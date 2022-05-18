967f4de55bce423acbf8252cbf1d49059ed51fe9
Toni Erdmann <Toni.Erdmann@web.de>
2022-05-18 20:55:17 +0200

    unset option, extend serach area

--- UTC-04/CL/RM/CL-RM-DTPM/settings.sh
+++ UTC-04/CL/RM/CL-RM-DTPM/settings.sh
@@ -8,7 +8,7 @@ PREFIX="CL-RM-DTPM"
 
 PTNA_TIMEZONE="America/Santiago"
 
-OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata~'^Q45632$'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[name='Gran Santiago'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Red Metropolitana de Movilidad"
 NETWORK_SHORT="DTPM"
 
@@ -16,8 +16,9 @@ ANALYSIS_PAGE="Chile/Public_Transport/Analysis"
 ANALYSIS_TALK="Talk:Chile/Public_Transport/Analysis/CL-DTPM-Routes"
 WIKI_ROUTES_PAGE="Chile/Public_Transport/Analysis/CL-DTPM-Routes"
 
-ANALYSIS_OPTIONS="--language=es --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-bus-stop --check-roundabouts --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=es --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-bus-stop --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
+#  --check-roundabouts
 # --expect-network-short
 # --expect-network-long
 # --expect-network-short-for=
@@ -31,7 +32,7 @@ ANALYSIS_OPTIONS="--language=es --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 
 # Name + Link to Overpass-Turbo call to show area on map
 PTNA_WWW_REGION_NAME="Santiago de Chile"
-PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5B%22wikidata%22~%22^Q45632$%22%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Btype%3Dboundary%5D%5Bname%3D%27Gran%20Santiago%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
 PTNA_WWW_NETWORK_NAME="Red Metropolitana de Movilidad"
