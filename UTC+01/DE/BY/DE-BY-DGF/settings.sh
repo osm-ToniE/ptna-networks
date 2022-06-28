adf8eea40bd81e3f4c31579254dd31adc84b02f8
Toni Erdmann <Toni.Erdmann@web.de>
2022-06-28 11:55:52 +0200

    add name and link to public transport information

--- UTC+01/DE/BY/DE-BY-DGF/settings.sh
+++ UTC+01/DE/BY/DE-BY-DGF/settings.sh
@@ -35,8 +35,8 @@ PTNA_WWW_REGION_NAME="Landkreis Dingolfing-Landau"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname%3D%27Landkreis%20Dingolfing-Landau%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Dingolfinger Regionalbusnetz"
-PTNA_WWW_NETWORK_LINK=""
+PTNA_WWW_NETWORK_NAME="Fahrplan/Öffentlicher Personennahverkehr"
+PTNA_WWW_NETWORK_LINK="https://www.landkreis-dingolfing-landau.de/verwaltung/fahrplaene-oepnv/oepnv/"
 
 # Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
