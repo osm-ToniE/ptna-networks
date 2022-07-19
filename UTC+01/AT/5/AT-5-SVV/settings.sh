bcb6d6d206ecceab42614d77a196104282eb2c50
Toni Erdmann <Toni.Erdmann@web.de>
2022-07-19 14:17:52 +0200

    add Austrian networks

--- UTC+01/AT/5/AT-5-SVV/settings.sh
+++ UTC+01/AT/5/AT-5-SVV/settings.sh
@@ -4,17 +4,17 @@
 # set variables for analysis of network
 #
 
-PREFIX="AT-6-VVSt-Graz"
+PREFIX="AT-5-SVV"
 
 PTNA_TIMEZONE="Europe/Vienna"
 
-OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata~'^Q13298$'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="S-Bahn Steiermark|Verkehrsverbund Steiermark"
-NETWORK_SHORT="VVSt"
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata~'^Q43325$'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Salzburger Verkehrsverbund"
+NETWORK_SHORT="SVV"
 
-ANALYSIS_PAGE="Graz/PTNA/Analyse"
-ANALYSIS_TALK="Talk:Graz/PTNA/Analyse"
-WIKI_ROUTES_PAGE="Graz/PTNA/VVSt-Linien-gesamt"
+ANALYSIS_PAGE="Austria/Nahverkehr_Salzburg/Analyse"
+ANALYSIS_TALK="Talk:Austria/Nahverkehr_Salzburg/Analyse"
+WIKI_ROUTES_PAGE="Austria/Nahverkehr_Salzburg/Analyse/SVV-Linien"
 
 ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
@@ -31,12 +31,12 @@ ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Statutarstadt Graz, Steiermark"
-PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^Q13298$'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+PTNA_WWW_REGION_NAME="Bundesland Salzburg"
+PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^Q43325$'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Verkehrsverbund Steiermark (Verbund Linie)"
-PTNA_WWW_NETWORK_LINK="https://www.verbundlinie.at/"
+PTNA_WWW_NETWORK_NAME="Salzburger Verkehrsverbund"
+PTNA_WWW_NETWORK_LINK="https://salzburg-verkehr.at/"
 
 # Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
@@ -49,5 +49,5 @@ PTNA_WWW_DISCUSSION_NAME="Diskussion"
 PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME="Grazer VVSt Linien"
+PTNA_WWW_ROUTES_NAME="SVV Linien"
 PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
--- UTC+01/AT/7/AT-7-VVT/settings.sh
+++ UTC+01/AT/7/AT-7-VVT/settings.sh
@@ -31,7 +31,7 @@ ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Tirol"
+PTNA_WWW_REGION_NAME="Bundesland Tirol"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^Q42880$'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
