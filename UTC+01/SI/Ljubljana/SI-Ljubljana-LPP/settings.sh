5522421517d0e15e6d4df56d86b8ede1a058a5a5
Toni Erdmann <Toni.Erdmann@web.de>
2023-01-12 22:54:08 +0100

    rename SI-Lubljana-MPP to *-LPP

--- UTC+01/SI/Ljubljana/SI-Ljubljana-LPP/settings.sh
+++ UTC+01/SI/Ljubljana/SI-Ljubljana-LPP/settings.sh
@@ -4,19 +4,19 @@
 # set variables for analysis of network
 #
 
-PREFIX="SI-Ljubljana-MPP"
+PREFIX="SI-Ljubljana-LPP"
 
-PTNA_TIMEZONE="Europe/Berlin"
+PTNA_TIMEZONE="Europe/Ljubljana"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=7][name='Upravna Enota Ljubljana'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Medkrajevni potniški promet|MPP Ljubljana - Urbana"
-NETWORK_SHORT="LPP|MPP"
+NETWORK_LONG="Medkrajevni potniški promet|MPP Ljubljana - Urbana|Ljubljanski Potniški Promet"
+NETWORK_SHORT="MPP|LPP"
 
 ANALYSIS_PAGE="Bus_routes_in_Ljubljana/Analiza"
 ANALYSIS_TALK="Talk:Bus_routes_in_Ljubljana/Analiza"
 WIKI_ROUTES_PAGE="Bus_routes_in_Ljubljana/Ljubljana-routes"
 
-ANALYSIS_OPTIONS="--language=sl --check-gtfs --link-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-route-ref --check-way-type --check-service-type --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=sl --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-route-ref --check-way-type --check-service-type --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
 # --show-gtfs
 # --check-bus-stop
@@ -36,8 +36,8 @@ PTNA_WWW_REGION_NAME="Ljubljana / Slovenija"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D7%5D%5Bname=%27Upravna Enota Ljubljana%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Medkrajevni potniški promet"
-PTNA_WWW_NETWORK_LINK="https://www.lpp.si/nova-stran-vozni-redi-medkrajevni-potniski-promet"
+PTNA_WWW_NETWORK_NAME="Ljubljanski Potniški Promet"
+PTNA_WWW_NETWORK_LINK="https://www.lpp.si/"
 
 # Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
