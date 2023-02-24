ad598cc661c64db26f15d6c05222212f50577e54
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-24 17:05:02 +0100

    add town of Memmingen to area of VVM

--- UTC+01/DE/BY/DE-BY-VVM-Mittelschwaben/settings.sh
+++ UTC+01/DE/BY/DE-BY-VVM-Mittelschwaben/settings.sh
@@ -8,7 +8,7 @@ PREFIX="DE-BY-VVM-Mittelschwaben"
 
 PTNA_TIMEZONE="Europe/Berlin"
 
-OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name~'(Landkreis Günzburg|Landkreis Unterallgäu)'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][wikidata~'^(Q10408|Q10412|Q14825)$'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Verkehrsverbund Mittelschwaben"
 NETWORK_SHORT="VVM"
 
@@ -31,8 +31,8 @@ ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Landkreis Günzburg und Landkreis Unterallgäu"
-PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bname~%27(Landkreis%20G%C3%BCnzburg%7CLandkreis%20Unterallg%C3%A4u)%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+PTNA_WWW_REGION_NAME="Kreisfreie Stadt Memmingen, Landkreise Günzburg und Unterallgäu"
+PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Bwikidata~'^(Q10408|Q10412|Q14825)$'%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
 PTNA_WWW_NETWORK_NAME="Verkehrsverbund Mittelschwaben"
