e574cb3a5b7d40387221086cd742ad6c77de1fb5
Toni Erdmann <Toni.Erdmann@web.de>
2023-04-03 08:57:46 +0200

    set expect-network-long-for/as

--- UTC+01/DE/NI/DE-NI-VGC/settings.sh
+++ UTC+01/DE/NI/DE-NI-VGC/settings.sh
@@ -9,14 +9,14 @@ PREFIX="DE-NI-VGC"
 PTNA_TIMEZONE="Europe/Berlin"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name='Landkreis Cloppenburg'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Verkehrsgemeinschaft Landkreis Cloppenburg"
+NETWORK_LONG="Verkehrsgemeinschaft Landkreis Cloppenburg|Verkehrsgemeinschaft Cloppenburg"
 NETWORK_SHORT="VGC"
 
 ANALYSIS_PAGE="Cloppenburg/Transportation/Analyse"
 ANALYSIS_TALK="Talk:Cloppenburg/Transportation/Analyse"
 WIKI_ROUTES_PAGE="Cloppenburg/Transportation/Analyse/DE-NI-VGC-Linien"
 
-ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=allow --positive-notes --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=de --expect-network-long-for='Verkehrsgemeinschaft Cloppenburg' --expect-network-long-as='Verkehrsgemeinschaft Landkreis Cloppenburg' --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=allow --positive-notes --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
 # --check-bus-stop
 # --expect-network-long
