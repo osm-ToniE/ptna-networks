2d488835b9ff4adc14219d3880fca7c1e736a450
Toni Erdmann <Toni.Erdmann@web.de>
2023-06-16 13:22:02 +0200

    remove --check-platform from options
    bus=yes is not required by PTv2

--- UTC-06/US/WI/US-WI-MCTS/settings.sh
+++ UTC-06/US/WI/US-WI-MCTS/settings.sh
@@ -16,8 +16,9 @@ ANALYSIS_PAGE="MCTS/Analysis"
 ANALYSIS_TALK="Talk:MCTS/Analysis"
 WIKI_ROUTES_PAGE="MCTS/MCTS_PTNA_Configuration"
 
-ANALYSIS_OPTIONS="--check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --allow-coach --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --max-error=20 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --check-bus-stop --check-platform --check-roundabouts"
+ANALYSIS_OPTIONS="--check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --allow-coach --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --max-error=20 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --check-bus-stop --check-roundabouts"
 
+# --check-platform
 # --expect-network-long
 # --expect-network-short
 # --expect-network-short-for=
