8bd4b93df7cbffc0d8ece34053f43e2cff0d3015
Toni Erdmann <Toni.Erdmann@web.de>
2023-07-25 20:43:12 +0200

    fix typo

--- UTC+01/DE/NI/DE-NI-VNN/settings.sh
+++ UTC+01/DE/NI/DE-NI-VNN/settings.sh
@@ -35,8 +35,8 @@ PTNA_WWW_REGION_NAME="Landkreise Cuxhaven, Harburg, Heidekreis, Lüchow-Dannenbe
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bwikidata~'^(Q5897|Q5907|Q5922|Q5913|Q5910|Q5923|Q5906|Q5915)$'%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Verkehrsgemeinschaft_Nordost-Niedersachsen;"
-PTNA_WWW_NETWORK_LINK="https://www.vnn.de/start; und andere"
+PTNA_WWW_NETWORK_NAME="Verkehrsgemeinschaft_Nordost-Niedersachsen; und andere"
+PTNA_WWW_NETWORK_LINK="https://www.vnn.de/start;"
 
 # Date and Time of last analysis in UTC and Local Time format
 # automatically build by PHP script
