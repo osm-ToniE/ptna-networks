833ab20143bb50557460aa6ea23257e004fd21c5
Toni Erdmann <Toni.Erdmann@web.de>
2023-07-28 16:44:18 +0200

    add missing blank

--- UTC-05/US/NY/MTA/settings.sh
+++ UTC-05/US/NY/MTA/settings.sh
@@ -34,7 +34,7 @@ ANALYSIS_OPTIONS="--check-gtfs --link-gtfs --show-gtfs --check-access --check-wa
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="New York City, NewYork"
+PTNA_WWW_REGION_NAME="New York City, New York"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5Bwikidata='Q60'%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
