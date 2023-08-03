e1b68d39109041d5de1b217ac2e7ce07d13711d1
Toni Erdmann <Toni.Erdmann@web.de>
2023-08-03 17:49:59 +0200

    extend list of Bürgerbusse

--- UTC+01/DE/NI/DE-NI-CeBus/settings.sh
+++ UTC+01/DE/NI/DE-NI-CeBus/settings.sh
@@ -9,7 +9,7 @@ PREFIX="DE-NI-CeBus"
 PTNA_TIMEZONE="Europe/Berlin"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][wikidata='Q5919'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Bürgerbus Bergen und Lohheide|Bürgerbus Eschede|Bürgerbus Faßberg|Bürgerbus Südheide|Bürgerbus Wietze"
+NETWORK_LONG="Bürgerbus Bergen und Lohheide|Bürgerbus Eschede|Bürgerbus Faßberg|Bürgerbus Flotwedel|Bürgerbus Südheide|Bürgerbus Wietze|Bürgerbus Winsen (Aller)"
 NETWORK_SHORT="CeBus"
 
 ANALYSIS_PAGE="Landkreis_Celle/Transportation/Analyse"
