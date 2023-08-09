213e2a238c239c4a6ebf3b03052073d1862b90de
Toni Erdmann <Toni.Erdmann@web.de>
2023-08-09 22:25:23 +0200

    fix typo

--- UTC+03.30/IR/07/IR-07-Shiraz/settings.sh
+++ UTC+03.30/IR/07/IR-07-Shiraz/settings.sh
@@ -50,5 +50,5 @@ PTNA_WWW_DISCUSSION_NAME="بحث / Discussion"
 PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME="خطوط شیراز/ Shiraz Routes"
+PTNA_WWW_ROUTES_NAME="خطوط شیراز / Shiraz Lines"
 PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
