71aff7a6447c5419a08a4a3b20ca4895bc7d8d21
Toni Erdmann <Toni.Erdmann@web.de>
2023-10-19 16:55:20 +0200

    extend PTNA_WWW_REGION_NAME

--- UTC+01/DE/BY/DE-BY-MVV/settings.sh
+++ UTC+01/DE/BY/DE-BY-MVV/settings.sh
@@ -33,7 +33,7 @@ ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --gtfs-feed=$PREFIX --m
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Region München"
+PTNA_WWW_REGION_NAME="Region München (inklusive im Dezember 2023 hinzukommende Landkreise)"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Bwikidata~'^(Q1726|Q10500|Q10544|Q10541|Q10539|Q10504|Q10497|Q10522|Q10502|Q4007|Q10526|Q10523)$'%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 #PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Bwikidata~'^(Q1726|Q10500|Q10544|Q10541|Q10539|Q10504|Q10497|Q10522|Q10502)$'%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
