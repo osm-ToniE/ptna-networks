459578a025896b7ec2ffa3bd3e654566e026a288
Toni Erdmann <Toni.Erdmann@web.de>
2023-11-09 21:56:07 +0100

    adapt to new location in OSM wiki
    - fix pseudo namespace Iran:*

--- UTC+03.30/IR/07/IR-07-Shiraz/settings.sh
+++ UTC+03.30/IR/07/IR-07-Shiraz/settings.sh
@@ -12,9 +12,9 @@ OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata='Q6397
 NETWORK_LONG=""
 NETWORK_SHORT=""
 
-ANALYSIS_PAGE="Iran:Fars:Shiraz/Public_Transport/Analysis"
-ANALYSIS_TALK="Talk:Iran:Fars:Shiraz/Public_Transport/Analysis"
-WIKI_ROUTES_PAGE="Iran:Fars:Shiraz/Public_Transport/Analysis/Routes"
+ANALYSIS_PAGE="Shiraz/Public_Transport/Analysis"
+ANALYSIS_TALK="Talk:Shiraz/Public_Transport/Analysis"
+WIKI_ROUTES_PAGE="Shiraz/Public_Transport/Analysis/Routes"
 
 ANALYSIS_OPTIONS="--language=fa --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train,light_rail,subway --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
 
