264b5b10c8ac1bc442aca2f1d51599ad4c0f9865
Toni Erdmann <Toni.Erdmann@web.de>
2023-11-15 19:50:32 +0100

    disable --check-platform --check-roundabouts

--- UTC+01/FR/GES/FR-GES-CTS/settings.sh
+++ UTC+01/FR/GES/FR-GES-CTS/settings.sh
@@ -17,13 +17,15 @@ ANALYSIS_PAGE="Strasbourg/CTS/Analyse"
 ANALYSIS_TALK="Talk:Strasbourg/CTS/Analyse"
 WIKI_ROUTES_PAGE="Strasbourg/CTS/PTNA"
 
-ANALYSIS_OPTIONS="--language=fr --check-bus-stop --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-platform --check-roundabouts --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=fr --check-bus-stop --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
 # --check-gtfs
 # --expect-network-short
 # --expect-network-short-for=
 # --expect-network-long-for=
 # --positive-notes
+# --check-platform
+# --check-roundabouts
 
 #
 # extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
