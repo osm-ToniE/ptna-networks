3750650bae89baf1f7048485e2e89f691dbcce0d
Toni Erdmann <Toni.Erdmann@web.de>
2023-12-01 00:15:08 +0100

    fix typo

--- UTC+01/DE/MV/DE-MV-VLP/settings.sh
+++ UTC+01/DE/MV/DE-MV-VLP/settings.sh
@@ -31,7 +31,7 @@ ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Landkreises Ludwigslust-Parchim, Amt Neuhaus im Landkreis Lüneburg"
+PTNA_WWW_REGION_NAME="Landkreis Ludwigslust-Parchim, Amt Neuhaus im Landkreis Lüneburg"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Bwikidata~%27^(Q2905|Q479687)$%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
