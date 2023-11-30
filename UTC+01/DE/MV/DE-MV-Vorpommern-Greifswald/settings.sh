469ceced5e1725e51a30bfe461dd1c7ec424c7af
Toni Erdmann <Toni.Erdmann@web.de>
2023-11-30 23:10:18 +0100

    add analysis for Vorpommern-Greifswald

---
+++ UTC+01/DE/MV/DE-MV-Vorpommern-Greifswald/settings.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="DE-MV-Vorpommern-Greifswald"
+
+PTNA_TIMEZONE="Europe/Berlin"
+
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][wikidata='Q2917'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG=""
+NETWORK_SHORT=""
+
+ANALYSIS_PAGE="Landkreis_Vorpommern-Greifswald/ÖPNV/PTNA"
+ANALYSIS_TALK="Talk:Landkreis_Vorpommern-Greifswald/ÖPNV/PTNA"
+WIKI_ROUTES_PAGE="Landkreis_Vorpommern-Greifswald/ÖPNV/PTNA-Linien"
+
+ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --coloured-sketchline --multiple-ref-type-entries=analyze --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+
+# --check-bus-stop
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Landkreis Vorpommern-Greifswald"
+PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bwikidata=%27Q2917%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME=""
+PTNA_WWW_NETWORK_LINK=""
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Diskussion"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="PTNA Linien"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
