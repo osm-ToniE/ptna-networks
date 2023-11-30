8455fb82c551dcf062af0a5b1fa827358f2364d4
Toni Erdmann <Toni.Erdmann@web.de>
2023-11-30 23:50:22 +0100

    rename 'network'

--- UTC+01/DE/MV/DE-MV-KGVP/settings.sh
+++ UTC+01/DE/MV/DE-MV-KGVP/settings.sh
@@ -4,7 +4,7 @@
 # set variables for analysis of network
 #
 
-PREFIX="DE-MV-Vorpommern-Greifswald"
+PREFIX="DE-MV-KGVP"
 
 PTNA_TIMEZONE="Europe/Berlin"
 
@@ -35,7 +35,7 @@ PTNA_WWW_REGION_NAME="Landkreis Vorpommern-Greifswald"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Badmin_level%3D6%5D%5Bwikidata=%27Q2917%27%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME=""
+PTNA_WWW_NETWORK_NAME='Kooperationsgemeinschaft „Vorpommern“'
 PTNA_WWW_NETWORK_LINK=""
 
 # Date and Time of last analysis in UTC and Local Time format
@@ -49,5 +49,5 @@ PTNA_WWW_DISCUSSION_NAME="Diskussion"
 PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME="PTNA Linien"
+PTNA_WWW_ROUTES_NAME="KGVP Linien"
 PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
