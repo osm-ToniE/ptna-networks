cf11f5a47994d4318e57f03a862c89e6a560fb94
Toni Erdmann <Toni.Erdmann@web.de>
2023-12-19 14:41:38 +0100

    disable WWW_ROUTES

--- UTC+02/EE/EE-Koik/settings.sh
+++ UTC+02/EE/EE-Koik/settings.sh
@@ -53,5 +53,5 @@ PTNA_WWW_DISCUSSION_NAME="Konfiguratsioon"
 PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME="Routes in Eesti"
-PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
+PTNA_WWW_ROUTES_NAME="" # "Routes in Eesti"
+PTNA_WWW_ROUTES_LINK="" # "https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
