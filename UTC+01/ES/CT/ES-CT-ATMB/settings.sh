fadb24cea932cf5ff854aa4b299205387835deb4
Toni Erdmann <Toni.Erdmann@web.de>
2024-01-03 18:40:31 +0100

    rename network

--- UTC+01/ES/CT/ES-CT-ATMB/settings.sh
+++ UTC+01/ES/CT/ES-CT-ATMB/settings.sh
@@ -4,17 +4,17 @@
 # set variables for analysis of network
 #
 
-PREFIX="ES-CT-B-ATM"
+PREFIX="ES-CT-ATMB"
 
 PTNA_TIMEZONE="Europe/Madrid"
 
 OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[wikidata=Q81949][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Autoritat del Transport Metropolità"
-NETWORK_SHORT="ATM"
+NETWORK_LONG="Autoritat del Transport Metropolità de l’Àrea de Barcelona"
+NETWORK_SHORT="ATM Barcelona"
 
 ANALYSIS_PAGE="Ca:Catalunya/Transport_públic/PTNA"
 ANALYSIS_TALK="Talk:Ca:Catalunya/Transport_públic/PTNA"
-WIKI_ROUTES_PAGE="Ca:Catalunya/Transport_públic/PTNA/Rutes_de_ATM"
+WIKI_ROUTES_PAGE="Ca:Catalunya/Transport_públic/PTNA/Rutes_de_ATM_Barcelona"
 
 ANALYSIS_OPTIONS="--language=ca --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
@@ -36,7 +36,7 @@ PTNA_WWW_REGION_NAME="Província de Barcelona, Catalunya"
 PTNA_WWW_REGION_LINK="http://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5B%22wikidata%22%3D%22Q81949%22%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Autoritat del Transport Metropolità"
+PTNA_WWW_NETWORK_NAME="Autoritat del Transport Metropolità de l’Àrea de Barcelona"
 PTNA_WWW_NETWORK_LINK="https://www.atm.cat/"
 
 # Date and Time of last analysis in UTC and Local Time format
@@ -50,5 +50,5 @@ PTNA_WWW_DISCUSSION_NAME="Discussion"
 PTNA_WWW_DISCUSSION_LINK="" # https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME="ATM Rutes"
+PTNA_WWW_ROUTES_NAME="ATMB Rutes"
 PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
