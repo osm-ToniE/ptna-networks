f1dba83070cd959455c15801e7065c13d21737e9
Toni Erdmann <Toni.Erdmann@web.de>
2024-07-08 14:15:25 +0200

    fix typo

--- UTC+01/ES/CT/ES-CT-ATMT/settings.sh
+++ UTC+01/ES/CT/ES-CT-ATMT/settings.sh
@@ -9,7 +9,7 @@ PREFIX="ES-CT-ATMT"
 PTNA_TIMEZONE="Europe/Madrid"
 
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata=Q98392][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="Autoritat del Transport Metropolità del Camp de tarragona"
+NETWORK_LONG="Autoritat del Transport Metropolità del Camp de Tarragona"
 NETWORK_SHORT="ATM Camp de Tarragona"
 
 ANALYSIS_PAGE="Ca:Catalunya/Transport_públic/PTNA"
