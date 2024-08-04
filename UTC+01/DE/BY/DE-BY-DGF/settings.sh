e2cbb873e1578c6718dff974aeb1410b603f9c90
Toni Erdmann <Toni.Erdmann@web.de>
2024-08-04 20:45:12 +0200

    enable 'use planet extract' for some more 'networks'

--- UTC+01/DE/BY/DE-BY-DGF/settings.sh
+++ UTC+01/DE/BY/DE-BY-DGF/settings.sh
@@ -8,6 +8,10 @@ PREFIX="DE-BY-DGF"
 
 PTNA_TIMEZONE="Europe/Berlin"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file will be filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...')
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+
+# just in case that using the planet extract file failed
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name='Landkreis Dingolfing-Landau'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Dingolfinger Regionalbusnetz"
 NETWORK_SHORT="DGF"
