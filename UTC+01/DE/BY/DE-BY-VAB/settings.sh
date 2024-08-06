86968569558bce5ea872670dc5a8726a5fe44f6c
Toni Erdmann <Toni.Erdmann@web.de>
2024-08-06 17:38:33 +0200

    add some more candidates for "use planet extract"

--- UTC+01/DE/BY/DE-BY-VAB/settings.sh
+++ UTC+01/DE/BY/DE-BY-VAB/settings.sh
@@ -8,6 +8,10 @@ PREFIX="DE-BY-VAB"
 
 PTNA_TIMEZONE="Europe/Berlin"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file will be filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...')
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+
+# just in case that using the planet extract file failed
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[boundary=public_transport][name='Verkehrsgemeinschaft%20am%20Bayerischen%20Untermain'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Verkehrsgemeinschaft am Bayerischen Untermain|Verkehrsgemeinschaft am Bayerischen Untermain (VAB)"
 NETWORK_SHORT="VAB"
