e48f2d8aa5e722595504d7bc0d9b00e9ccc2a537
Toni Erdmann <Toni.Erdmann@web.de>
2024-08-10 14:31:28 +0200

    add 'network' US-WA-KCM

---
+++ UTC-08/US/WA/US-WA-KCM/settings.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="US-WA-KCM"
+
+PTNA_TIMEZONE="America/Los_Angeles"
+
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata='Q108861'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="King County Metro"
+NETWORK_SHORT="KCM"
+
+ANALYSIS_PAGE="Washington/Public_Transport/King_County_Metro/PTNA/Analysis"
+ANALYSIS_TALK="Talk:Washington/Public_Transport/King_County_Metro/PTNA/Analysis"
+WIKI_ROUTES_PAGE="Washington/Public_Transport/King_County_Metro/PTNA/Analysis/Routes"
+
+ANALYSIS_OPTIONS="--language=en --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-bus-stop --check-roundabouts --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --max-error=10 --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+
+# --expect-network-short
+# --expect-network-long
+# --expect-network-short-for=
+# --expect-network-long-for=
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="King County, Washington"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5B%22wikidata%22=%22Q108861%22%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="King County Metro"
+PTNA_WWW_NETWORK_LINK="https://kingcounty.gov/en/dept/metro"
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Discussion"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="KCM Routes"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
