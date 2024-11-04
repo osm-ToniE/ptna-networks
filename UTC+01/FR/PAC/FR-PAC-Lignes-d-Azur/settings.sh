6c7e810c4955ac2d38a396455027023d7accf2ff
Toni Erdmann <Toni.Erdmann@web.de>
2024-11-04 15:15:46 +0100

    add --show-gtfs; remove --expect-network-long

--- UTC+01/FR/PAC/FR-PAC-Lignes-d-Azur/settings.sh
+++ UTC+01/FR/PAC/FR-PAC-Lignes-d-Azur/settings.sh
@@ -20,7 +20,7 @@ ANALYSIS_PAGE="Nice/Transports_en_commun/Analyse"
 ANALYSIS_TALK="Talk:Nice/Transports_en_commun/Analyse"
 WIKI_ROUTES_PAGE="Nice/Transports_en_commun/Lignes_Nice"
 
-ANALYSIS_OPTIONS="--language=fr --check-bus-stop --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-platform --check-roundabouts --expect-network-long --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--language=fr --check-bus-stop --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-platform --check-roundabouts --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
 # --check-gtfs
 # --expect-network-short
