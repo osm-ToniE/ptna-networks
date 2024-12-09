626e690e3174067e19ddc3f0cd05a25452a7fb5f
Toni Erdmann <Toni.Erdmann@web.de>
2024-12-09 12:33:44 +0100

    mark "RoVG" as being delete 2025-01-01
    - joined MVV end of 2023

--- UTC+01/DE/BY/DE-BY-RoVG/settings.sh
+++ UTC+01/DE/BY/DE-BY-RoVG/settings.sh
@@ -40,7 +40,7 @@ PTNA_WWW_REGION_NAME="Stadt und Landkreis Rosenheim"
 PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Bwikidata~%22^(Q4007|Q10526)$%22%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Rosenheimer Verkehrsgesellschaft"
+PTNA_WWW_NETWORK_NAME="Rosenheimer Verkehrsgesellschaft (wird zum 2025-01-01 aufgelöst)"
 PTNA_WWW_NETWORK_LINK="https://rovg.de/"
 
 # Date and Time of last analysis in UTC and Local Time format
