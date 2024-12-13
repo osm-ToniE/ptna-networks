71ff7f0ebdeff2a9e9a2a7a36d5f6cd6656e2b49
Toni Erdmann <Toni.Erdmann@web.de>
2024-12-13 17:46:17 +0100

    add more 'use extract' config information

--- UTC+01/DE/TH/DE-TH-VMT/settings.sh
+++ UTC+01/DE/TH/DE-TH-VMT/settings.sh
@@ -8,6 +8,9 @@ PREFIX="DE-TH-VMT"
 
 PTNA_TIMEZONE="Europe/Berlin"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[boundary=administrative][wikidata~'^(Q1729|Q3955|Q3150|Q3750|Q7869|Q7879|Q7884|Q7882|Q7883)$'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Verkehrsverbund Mittelthüringen"
 NETWORK_SHORT="VMT"
