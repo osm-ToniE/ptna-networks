267030f1bc3eed187f38d3d7344130390956c34c
Toni Erdmann <Toni.Erdmann@web.de>
2024-12-14 18:17:49 +0100

    add 'use extract' configuration data for 'Lower Saxony, Germany'

--- UTC+01/DE/NI/DE-NI-VGE/settings.sh
+++ UTC+01/DE/NI/DE-NI-VGE/settings.sh
@@ -8,6 +8,9 @@ PREFIX="DE-NI-VGE"
 
 PTNA_TIMEZONE="Europe/Berlin"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name='Landkreis Emsland'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Verkehrsgemeinschaft Emsland-Süd"
 NETWORK_SHORT="VGE-Süd|VGE"
