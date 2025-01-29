f1b6755d5fdfea2a7d21131dffd47e5a50be50ff
Patchi-Osm <76591095+Patchi-Osm@users.noreply.github.com>
2025-01-29 07:21:14 +0100

    Update network list
    Made an error with the last change. The goal was to update the websites, but I made also one change in the network list which was wrong. So getting back the right network list.

--- UTC+01/FR/PAC/FR-PAC-Vaucluse/settings.sh
+++ UTC+01/FR/PAC/FR-PAC-Vaucluse/settings.sh
@@ -13,7 +13,7 @@ PTNA_TIMEZONE="Europe/Paris"
 
 # Use the Wikidata boundary of the Vaucluse département
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:600];area[wikidata=Q12792][type=boundary];(rel(area)[~'route'~'bus'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
-NETWORK_LONG="TUB Bollène|Trans'CoVe|TCVO|Sorg'en bus|C mon bus|La Métropole Mobilité|Mobily"
+NETWORK_LONG="TUB Bollène|Trans'CoVe|TCVO|Sorg'en bus|C mon bus|La Métropole Mobilité - LeBus|La Métropole Mobilité - LeCar|Mobily"
 NETWORK_SHORT=""
 
 ANALYSIS_PAGE="Vaucluse/Transports_en_commun/Analyse"
