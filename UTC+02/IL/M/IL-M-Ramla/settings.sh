de758fbac2a706d7d2cac21292045dee1e84a5eb
Toni Erdmann <Toni.Erdmann@web.de>
2025-02-24 14:04:49 +0100

    fix errors in names of sub-districts

--- UTC+02/IL/M/IL-M-Ramla/settings.sh
+++ UTC+02/IL/M/IL-M-Ramla/settings.sh
@@ -4,20 +4,20 @@
 # set variables for analysis of network
 #
 
-PREFIX="IL-M-haSharon"
+PREFIX="IL-M-Ramla"
 
 PTNA_TIMEZONE="Asia/Jerusalem"
 
 # PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
 PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
 
-OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:900];area[wikidata='Q5422629'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:900];area[wikidata='Q5419070'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG=""
 NETWORK_SHORT=""
 
 ANALYSIS_PAGE="Israel/Public_Transport/PTNA"
 ANALYSIS_TALK="Talk:Israel/Public_Transport/PTNA"
-WIKI_ROUTES_PAGE="Israel/Public_Transport/PTNA/IL-M-haSharon-Routes"
+WIKI_ROUTES_PAGE="Israel/Public_Transport/PTNA/IL-M-Ramla-Routes"
 
 ANALYSIS_OPTIONS="--check-gtfs --link-gtfs --show-gtfs --gtfs-feed=IL-MOT --allow-coach --check-access --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --max-error=20 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --check-bus-stop --check-roundabouts"
 
@@ -35,8 +35,8 @@ ANALYSIS_OPTIONS="--check-gtfs --link-gtfs --show-gtfs --gtfs-feed=IL-MOT --allo
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="מחוז המרכז, נפת השרון / haSharon Subdistrict, Center District"
-PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5Bwikidata%3D'Q5422629'%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+PTNA_WWW_REGION_NAME="מחוז המרכז, נפת רמלה / Ramla Subdistrict, Center District"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5Bwikidata%3D'Q5419070'%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
 PTNA_WWW_NETWORK_NAME="All public transport associations"
@@ -53,5 +53,5 @@ PTNA_WWW_DISCUSSION_NAME="Discussion"
 PTNA_WWW_DISCUSSION_LINK= #"https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME="haSharon, Center District Routes"
+PTNA_WWW_ROUTES_NAME="Ramla, Center District Routes"
 PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
