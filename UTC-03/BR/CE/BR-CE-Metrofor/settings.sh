c5aff500009247e18e0847628afe3ec1d05dfa32
Toni Erdmann <Toni.Erdmann@web.de>
2025-02-24 13:18:39 +0100

    add network BR-CE-Metrofor

---
+++ UTC-03/BR/CE/BR-CE-Metrofor/settings.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="BR-CE-Metrofor"
+
+PTNA_TIMEZONE="America/Fortaleza"
+
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata~'^(Q43463|Q1794732|Q817233)$'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Companhia Cearense de Transportes Metropolitanos"
+NETWORK_SHORT="Metrofor"
+
+ANALYSIS_PAGE="Ceará/Metrofor/Análise"
+ANALYSIS_TALK="Talk:Ceará/Metrofor/Análise"
+WIKI_ROUTES_PAGE="Ceará/Metrofor/Análise/Metrofor_Lines"
+
+ANALYSIS_OPTIONS="--language=pt_BR --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-way-type --check-service-type --check-bus-stop --check-roundabouts --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --positive-notes --max-error=10 --multiple-ref-type-entries=allow --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+
+# --expect-network-short
+# --expect-network-long
+# --expect-network-short-for=
+# --expect-network-long-for=
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Municípios de Fortaleza, Caucaia e Maracanaú / Ceará"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5Bwikidata~'^(Q43463|Q1794732|Q817233)$'%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Companhia Cearense de Transportes Metropolitanos (Metrofor)"
+PTNA_WWW_NETWORK_LINK="https://www.metrofor.ce.gov.br/"
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Discussão"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="Metrofor Linhas"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
