eba12adaa07596726df72bfb234ede3d646616b7
Toni Erdmann <Toni.Erdmann@web.de>
2025-10-08 23:35:59 +0200

    add some more 'use extract' configs FR-GES-*, FR-NAQ-*

--- UTC+01/FR/GES/FR-GES-STAN/settings.sh
+++ UTC+01/FR/GES/FR-GES-STAN/settings.sh
@@ -8,6 +8,12 @@ PREFIX="FR-GES-STAN"
 
 PTNA_TIMEZONE="Europe/Paris"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="xx-departement-meurthe-et-moselle"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:600];area[wikidata='Q2989397'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Service de transport de l'agglomération nancéienne"
 NETWORK_SHORT="STAN|Stan"
@@ -30,7 +36,7 @@ ANALYSIS_OPTIONS="--language=fr --check-bus-stop --check-gtfs --link-gtfs --show
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Métropole du Grand Nancy"
+PTNA_WWW_REGION_NAME="Métropole du Grand Nancy, Meurthe-et-Moselle"
 PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=[out%3Ajson][timeout%3A25]%3B(relation[wikidata='Q2989397'][type%3Dboundary]%3B)%3Bout+body%3B%3E%3Bout+skel+qt%3B{{data%3Aoverpass%2Cserver%3D%2F%2Foverpass.openstreetmap.fr%2Fapi%2F}}"
 
 # Name + Link to the network provider / transport association
