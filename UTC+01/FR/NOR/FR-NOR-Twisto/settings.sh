30688d3abd16a31cb13835be243618414f1630cc
Toni Erdmann <Toni.Erdmann@web.de>
2025-10-08 17:28:35 +0200

    add some mor 'use extract' configs FR-NOR-*

--- UTC+01/FR/NOR/FR-NOR-Twisto/settings.sh
+++ UTC+01/FR/NOR/FR-NOR-Twisto/settings.sh
@@ -8,6 +8,12 @@ PREFIX="FR-NOR-Twisto"
 
 PTNA_TIMEZONE="Europe/Paris"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="zz-departement-calvados"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:600];area[wikidata='Q88681432'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Twisto"
 NETWORK_SHORT=""
@@ -31,7 +37,7 @@ ANALYSIS_OPTIONS="--language=fr --positive-notes --check-bus-stop --link-gtfs --
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Caen Normandie Métropole"
+PTNA_WWW_REGION_NAME="Caen Normandie Métropole, Calvados"
 PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=[out%3Ajson][timeout%3A25]%3B(relation[wikidata='Q88681432'][type%3Dboundary]%3B)%3Bout+body%3B%3E%3Bout+skel+qt%3B{{data%3Aoverpass%2Cserver%3D%2F%2Foverpass.openstreetmap.fr%2Fapi%2F}}"
 
 # Name + Link to the network provider / transport association
