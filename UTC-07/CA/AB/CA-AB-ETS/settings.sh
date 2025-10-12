f745a83f0475ebd1f3dc0dca2ee6c3a0e743d639
Toni Erdmann <Toni.Erdmann@web.de>
2025-10-12 16:57:01 +0200

    add network CA-AB-ETS

---
+++ UTC-07/CA/AB/CA-AB-ETS/settings.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="CA-AB-ETS"
+
+PTNA_TIMEZONE="America/Edmonton"
+
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:900];area[wikidata='Q1133254'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Edmonton Transit Service|Beaumont Transit|Edmonton LRT|Leduc Transit|Spruce Grove Transit|Strathcona County Transit|St. Albert Transit|Fort Saskatchewan Transit"
+NETWORK_SHORT="ETS|FST|StAT"
+
+ANALYSIS_PAGE="Edmonton/Metropolitan_Region/Public_Transit/Analysis"
+ANALYSIS_TALK="Talk:Edmonton/Metropolitan_Region/Public_Transit/Analysis"
+WIKI_ROUTES_PAGE="Edmonton/Metropolitan_Region/Public_Transit/Analysis/PTNA/ETS_Routes"
+
+ANALYSIS_OPTIONS="--check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --allow-coach --check-access --check-dates --check-way-type --check-service-type --check-sequence --check-version --check-osm-separator --check-motorway-link --max-error=20 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --check-bus-stop"
+
+# --check-platform
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+# --relaxed-begin-end-for=bus
+# --check-name-relaxed
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Edmonton Metropolitan Region, Alberta"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation%5Bwikidata%3D'Q1133254'%5D%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Edmonton Transit Service"
+PTNA_WWW_NETWORK_LINK="https://www.edmonton.ca/edmonton-transit-system-ets"
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Discussion"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="ETS Routes"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
