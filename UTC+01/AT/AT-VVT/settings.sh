375daaed0550a9582525b7bb002313a2bfd1dadf
Toni Erdmann <Toni.Erdmann@web.de>
2025-10-13 18:10:47 +0200

    add more 'ues extract' cofigs for AT-*

--- UTC+01/AT/AT-VVT/settings.sh
+++ UTC+01/AT/AT-VVT/settings.sh
@@ -8,6 +8,12 @@ PREFIX="AT-VVT"
 
 PTNA_TIMEZONE="Europe/Vienna"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="tirol"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata~'^Q42880$'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Verkehrsverbund Tirol|CityBus Wörgl|Dorfbus St. Anton|Innsbrucker Verkehrsbetriebe|Skibus See|Skibus Stubaier Gletscher|Stadtbus Hall|Stadtverkehr Kufstein|Stadtverkehr Lienz"
 NETWORK_SHORT="VVT|INKB|IVB"
