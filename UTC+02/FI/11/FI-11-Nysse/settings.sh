4387fb6d1be9a6b15b56f8eceea799d447179ce4
Toni Erdmann <Toni.Erdmann@web.de>
2025-10-20 18:12:52 +0200

    add network FI-11-Nysse

---
+++ UTC+02/FI/11/FI-11-Nysse/settings.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="FI-11-Nysse"
+
+PTNA_TIMEZONE="Europe/Tallinn"
+
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="pirkanmaa"
+
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata~'^(Q1787075)$'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Nysse"
+NETWORK_SHORT=""
+
+ANALYSIS_PAGE="Public_transport_in_Tampere/Analysis"
+ANALYSIS_TALK="Talk:Public_transport_in_Tampere/Analysis"
+WIKI_ROUTES_PAGE="Public_transport_in_Tampere/Analysis/PTNA/Nysse_Routes"
+
+ANALYSIS_OPTIONS="--language=en --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
+
+# --check-bus-stop
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+#
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Tampereen seutukunta / Tampere sub-region"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^(Q1787075)$'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Nysse"
+PTNA_WWW_NETWORK_LINK="https://www.nysse.fi/"
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Konfiguraatio"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="Nysse Routes"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
