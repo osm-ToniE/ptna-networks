2ccd530633e88f2e248be80b9f47ade5028685db
Toni Erdmann <Toni.Erdmann@web.de>
2025-11-27 16:18:40 +0100

    add network DE-SN-ZVVO providing service from 2026-01-01 on
    - VVO and ZVON join

---
+++ UTC+01/DE/SN/DE-SN-ZVVO/settings.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="DE-SN-ZVVO"
+
+PTNA_TIMEZONE="Europe/Berlin"
+
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="saxony"
+
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:900];area[wikidata~'^(Q1731|Q571947|Q6317|Q6313|Q6323)$'][boundary=administrative];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Zweckverband Verkehrsverbund Ostsachsen|Zweckverband Verkehrsverbund Ostsachsen GmbH|Verkehrsverbund Ostsachsen|Verkehrsverbund Ostsachsen GmbH|Verkehrsverbund Oberelbe|Verkehrsverbund Oberelbe GmbH|Zweckverband Verkehrsverbund Oberlausitz-Niederschlesien|Verkehrsverbund Oberlausitz-Niederschlesien"
+NETWORK_SHORT="ZVVO|VVO|ZVON"
+
+ANALYSIS_PAGE="Saxony/Transportation/Verkehrsverbund_Ostsachsen/Analyse"
+ANALYSIS_TALK="Talk:Saxony/Transportation/Verkehrsverbund_Ostsachsen/Analyse"
+WIKI_ROUTES_PAGE="Saxony/Transportation/Verkehrsverbund_Ostsachsen/Analyse/DE-SN-ZVVO-Linien"
+
+ANALYSIS_OPTIONS="--language=de --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+
+# --language=de
+# --check-bus-stop
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Stadt Dresden, Landkreise Bautzen, Görlitz, Meißen und Sächsische Schweiz-Osterzgebirge"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata~'^Q1731|Q571947|Q6317|Q6313|Q6323$'][boundary=administrative]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Verkehrsverbund Ostsachsen (ab 1.1.2026 als Zusammenschluss von VVO und ZVON)"
+PTNA_WWW_NETWORK_LINK=""
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Discussion"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="ZVVO Linien"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
