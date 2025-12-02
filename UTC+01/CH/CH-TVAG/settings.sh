c7700f11303ce1fad077c990bbdfd5576e795b2d
Toni Erdmann <Toni.Erdmann@web.de>
2025-12-02 17:10:35 +0100

    fix typo

--- UTC+01/CH/CH-TVAG/settings.sh
+++ UTC+01/CH/CH-TVAG/settings.sh
@@ -33,7 +33,7 @@ ANALYSIS_OPTIONS="--check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$GTFS_FEED --
 # automatically build by PHP script
 
 # Name + Link to Overpass-Turbo call to show area on map
-PTNA_WWW_REGION_NAME="Kanton Aargau (SZ) mit Ausnahme der Bezirke Laufenburg und Rheinfelden, Kanton Solothurn (SO) die östlichen Bezirke Gäu, Gösgen, Olten und Thal"
+PTNA_WWW_REGION_NAME="Kanton Aargau (AG) mit Ausnahme der Bezirke Laufenburg und Rheinfelden, Kanton Solothurn (SO) die östlichen Bezirke Gäu, Gösgen, Olten und Thal"
 PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D%5Bwikidata~%22^(Q121410|Q658994|Q659309|Q659597|Q598525|Q656740|Q659293|Q659715|Q659332|Q660753|Q660133|Q660290|Q598278)$%22%5D%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
