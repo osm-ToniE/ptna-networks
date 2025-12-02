69bfaab55d1dadf674af608b9e201f5460acced9
Toni Erdmann <Toni.Erdmann@web.de>
2025-12-02 17:10:51 +0100

    add some more configurations for 'use extract'

--- UTC+01/CH/CH-CTIFR/settings.sh
+++ UTC+01/CH/CH-CTIFR/settings.sh
@@ -9,6 +9,12 @@ GTFS_FEED="CH-Alle"
 
 PTNA_TIMEZONE="Europe/Zurich"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="xx-region-GE-FR-JU-NE-VD-VS"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[boundary=administrative][wikidata~'^(Q12640|Q495882)$'];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Communauté Tarifaire Intégrale Fribourgeoise|frimobil|Frimobil"
 NETWORK_SHORT="CTIFR"
