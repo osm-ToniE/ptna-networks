894cab28c19ed295e94dd4500752a341dff4bb43
Toni Erdmann <Toni.Erdmann@web.de>
2025-12-04 18:38:51 +0100

    fix typo

--- UTC+01/CH/CH-Vagabond/settings.sh
+++ UTC+01/CH/CH-Vagabond/settings.sh
@@ -56,5 +56,5 @@ PTNA_WWW_DISCUSSION_NAME="Discussion"
 PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
 
 # Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
-PTNA_WWW_ROUTES_NAME="vagABOnd Lines"
+PTNA_WWW_ROUTES_NAME="Vagabond Lines"
 PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
