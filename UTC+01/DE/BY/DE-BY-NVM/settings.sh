842277dec6b1c80eb67f5f79a0faa538ae4164d0
Toni Erdmann <Toni.Erdmann@web.de>
2025-12-08 16:58:36 +0100

    delete start date from PTNA_WWW_NETWORK_NAME

--- UTC+01/DE/BY/DE-BY-NVM/settings.sh
+++ UTC+01/DE/BY/DE-BY-NVM/settings.sh
@@ -42,7 +42,7 @@ PTNA_WWW_REGION_NAME="Stadt- und Landkreis Würzburg, Stadt- und Landkreis Schwe
 PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation%5Bboundary%3Dadministrative%5D[wikidata~'^(Q2999|Q10458|Q4126|Q10461|Q10466|Q10462|Q10459|Q10469|Q10465)$']%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
 
 # Name + Link to the network provider / transport association
-PTNA_WWW_NETWORK_NAME="Nahverkehr Mainfranken; beginnt am 2025-01-01"
+PTNA_WWW_NETWORK_NAME="Nahverkehr Mainfranken"
 PTNA_WWW_NETWORK_LINK="https://www.nahverkehr-mainfranken.de/"
 
 # Date and Time of last analysis in UTC and Local Time format
