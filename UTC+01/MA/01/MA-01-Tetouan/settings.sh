8b341218515fdb46dbafe9c4e6f997402306e5b5
Toni Erdmann <Toni.Erdmann@web.de>
2025-12-12 15:07:34 +0100

    add 'use extract' config for MA-*

--- UTC+01/MA/01/MA-01-Tetouan/settings.sh
+++ UTC+01/MA/01/MA-01-Tetouan/settings.sh
@@ -8,8 +8,12 @@ PREFIX="MA-01-Tetouan"
 
 PTNA_TIMEZONE="Africa/Casablanca"
 
-# avoid downloading same area/data if the data has already been downloaded and is not older than 1 hour (start analysis with: "ptna-networks.sh -fo" to 'f'orce download 'o'verpass data)
-# OVERPASS_REUSE_ID=""
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+#PTNA_EXTRACT_GETIDS="xx-region-tangier-tetouan-al-hoceima"
+PTNA_EXTRACT_GETIDS="yy-province-tetouan"
 
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata=Q185157][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[~'type'~'route'](r);)->.routes;(.routes;<<;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Vitalis Network|Vitalis Transport"
