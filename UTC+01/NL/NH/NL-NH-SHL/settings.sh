50fc9fe2deddbf55a44710242e0650107393550a
Toni Erdmann <Toni.Erdmann@web.de>
2025-12-13 17:44:23 +0100

    add more 'use extract' configs for NL-*

--- UTC+01/NL/NH/NL-NH-SHL/settings.sh
+++ UTC+01/NL/NH/NL-NH-SHL/settings.sh
@@ -8,6 +8,12 @@ PREFIX="NL-NH-SHL"
 
 PTNA_TIMEZONE="Europe/Amsterdam"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="haarlemmermeer"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata='Q9694'][type=multipolygon];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Schiphol Landside/Airside"
 NETWORK_SHORT=""
--- UTC+01/NL/NH/NL-NH-ZAWA/settings.sh
+++ UTC+01/NL/NH/NL-NH-ZAWA/settings.sh
@@ -8,6 +8,12 @@ PREFIX="NL-NH-ZAWA"
 
 PTNA_TIMEZONE="Europe/Amsterdam"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="amsterdam-shared.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="amsterdam"
+
 # avoid downloading same area/data if the data has already been downloaded and is not older than 1 hour (start analysis with: "ptna-networks.sh -fo" to 'f'orce download)
 OVERPASS_REUSE_ID="NL-NH-Q9899-all"
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata='Q9899'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
