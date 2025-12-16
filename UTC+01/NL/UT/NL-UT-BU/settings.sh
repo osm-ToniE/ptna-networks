a4332a7a54b9a585428e1c974125f4b06dc87dca
Toni Erdmann <Toni.Erdmann@web.de>
2025-12-16 19:22:58 +0100

    add networks NL-UT-BI and NL-UT-BU

---
+++ UTC+01/NL/UT/NL-UT-BU/settings.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="NL-UT-BU"
+
+PTNA_TIMEZONE="Europe/Amsterdam"
+
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="utrecht-shared.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="utrecht"
+
+# avoid downloading same area/data if the data has already been downloaded and is not older than 1 hour (start analysis with: "ptna-networks.sh -fo" to 'f'orce download)
+OVERPASS_REUSE_ID="NL-UT-Q776-all"
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata='Q776'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Utrect Buiten"
+NETWORK_SHORT=""
+
+ANALYSIS_PAGE="NL-OV/PTNA"
+ANALYSIS_TALK="Talk:NL-OV/PTNA"
+WIKI_ROUTES_PAGE="NL-OV/PTNA/Utrect_Buiten_lijnen"
+
+ANALYSIS_OPTIONS="--language=nl --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=NL-OVApi --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train,light_rail,tram --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
+
+# --check-bus-stop
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+#
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Provincie Utrecht"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata='Q776'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Concessie Utrecht Buiten"
+PTNA_WWW_NETWORK_LINK="https://www.u-ov.nl/"
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Configuratie"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="Utrecht Buiten lijnen"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
