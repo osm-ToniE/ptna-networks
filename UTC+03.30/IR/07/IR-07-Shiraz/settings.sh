689aafaae0068b9355a455d3e8af4881487f58ab
Toni Erdmann <Toni.Erdmann@web.de>
2026-01-01 17:19:35 +0100

    change language from fa to fa_IR

--- UTC+03.30/IR/07/IR-07-Shiraz/settings.sh
+++ UTC+03.30/IR/07/IR-07-Shiraz/settings.sh
@@ -16,7 +16,7 @@ ANALYSIS_PAGE="Shiraz/Public_Transport/Analysis"
 ANALYSIS_TALK="Talk:Shiraz/Public_Transport/Analysis"
 WIKI_ROUTES_PAGE="Shiraz/Public_Transport/Analysis/Routes"
 
-ANALYSIS_OPTIONS="--language=fa --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train,light_rail,subway --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
+ANALYSIS_OPTIONS="--language=fa_IR --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train,light_rail,subway --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
 
 # --check-bus-stop
 # --expect-network-long
