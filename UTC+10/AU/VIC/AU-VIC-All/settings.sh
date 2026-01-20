eb4d6a639a1940158bc149bcfbb523ea5d3a7244
Toni Erdmann <Toni.Erdmann@web.de>
2026-01-20 10:51:51 +0100

    add network AU-VIC-All

---
+++ UTC+10/AU/VIC/AU-VIC-All/settings.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="AU-VIC-All"
+
+PTNA_TIMEZONE="Australia/Melbourne"
+
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata='Q36687'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG=""
+NETWORK_SHORT=""
+
+ANALYSIS_PAGE="Victoria,_Australia/Public_transport/PTNA/Analysis"
+ANALYSIS_TALK="Talk:Victoria,_Australia/Public_transport/PTNA/Analysis"
+WIKI_ROUTES_PAGE="Victoria,_Australia/Public_transport/PTNA/Analysis/All_Routes"
+
+ANALYSIS_OPTIONS="--timezone=$PTNA_TIMEZONE --check-gtfs --link-gtfs --show-gtfs --check-from-to=relaxed --check-via=relaxed --check-access --check-dates --check-route-ref --check-way-type --check-service-type --check-stop-position --check-sequence --check-version --check-osm-separator --relaxed-begin-end-for=train,light_rail,tram --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
+
+# --check-motorway-link
+# --check-name-relaxed
+# --check-bus-stop
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+#
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Victoria, Australia"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata='Q36687'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="All public transport"
+PTNA_WWW_NETWORK_LINK=""
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Discussion"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="Victoria Routes"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
