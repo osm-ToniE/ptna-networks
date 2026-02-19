02d4401f348ca879f87457d5dd268a8f66f7a32c
Toni Erdmann <Toni.Erdmann@web.de>
2026-02-20 00:34:12 +0100

    add support for RO-SB-Tursib (GTFS, analysis)

---
+++ UTC+02/RO/SB/RO-SB-Tursib/settings.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="RO-SB-Tursib"
+
+PTNA_TIMEZONE="Europe/Bucharest"
+
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="county-sibiu"
+
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata='Q83324'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="Tursib"
+NETWORK_SHORT=""
+
+ANALYSIS_PAGE="Sibiu/Public_Transport/PTNA"
+ANALYSIS_TALK="Talk:Sibiu/Public_Transport/PTNA"
+WIKI_ROUTES_PAGE="Sibiu/Public_Transport/PTNA/Tursib-Linii"
+
+ANALYSIS_OPTIONS="--timezone=$PTNA_TIMEZONE --language=ro  --gtfs-feed=$PREFIX --check-gtfs --link-gtfs --show-gtfs --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --relaxed-begin-end-for=train --max-error=10 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline"
+
+# --check-bus-stop
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-for=
+# --gtfs-feed=$PREFIX
+# --check-gtfs --link-gtfs --show-gtfs
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Muncipiu Sibiu / City of Sibiu"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B(relation[wikidata='Q83324'][type=boundary]%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="Tursib"
+PTNA_WWW_NETWORK_LINK="https://tursib.ro/"
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Configurație"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="Tursib Linii"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
