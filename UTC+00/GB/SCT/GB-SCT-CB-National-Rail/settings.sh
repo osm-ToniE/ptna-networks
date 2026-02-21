aa938101604ca2fcbcf243175da5d5cd66fcf669
Toni Erdmann <Toni.Erdmann@web.de>
2026-02-21 13:16:32 +0100

    add network GB-SCT-CB-National-Rail

---
+++ UTC+00/GB/SCT/GB-SCT-CB-National-Rail/settings.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#
+# set variables for analysis of network
+#
+
+PREFIX="GB-SCT-CB-National-Rail"
+
+PTNA_TIMEZONE="Europe/London"
+
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="united-kingdom"
+
+OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:900];area[boundary=statistical][admin_level=6]['ref:itl:2'~'^(TLM1|TLM3)$'];(rel(area)[~'route'~'(train|light_rail|monorail)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
+NETWORK_LONG="National Rail"
+NETWORK_SHORT=""
+
+ANALYSIS_PAGE="Railways_in_the_United_Kingdom/PTNA/Analysis"
+ANALYSIS_TALK="Talk:Railways_in_the_United_Kingdom/PTNA/Analysis"
+WIKI_ROUTES_PAGE="Railways_in_the_United_Kingdom/PTNA/National_Rail/SCT_CB_Routes"
+
+ANALYSIS_OPTIONS="--timezone=$PTNA_TIMEZONE --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --max-error=20 --multiple-ref-type-entries=analyze --positive-notes --coloured-sketchline --check-bus-stop --check-roundabouts"
+
+# --allow-coach
+# --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX
+# --check-motorway-link
+# --check-platform
+# --expect-network-long
+# --expect-network-short
+# --expect-network-short-for=
+# --expect-network-long-ford
+# --relaxed-begin-end-for=bus
+
+#
+# extensions to support ptna-www and PHP in results/xx/index.php files by code in ptna-network.sh (section: upload results)
+#
+# Name + Link to Analysis Result Page on server
+# automatically build by PHP script
+
+# Name + Link to Overpass-Turbo call to show area on map
+PTNA_WWW_REGION_NAME="Central Belt, Scotland (East and West Central Scotland)"
+PTNA_WWW_REGION_LINK="https://overpass-turbo.eu/map.html?Q=%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%0A(%0A%0A%20%20relation[boundary=statistical][admin_level=6]['ref:itl:2'~'^(TLM1|TLM3)$')$']%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+
+# Name + Link to the network provider / transport association
+PTNA_WWW_NETWORK_NAME="National Rail"
+PTNA_WWW_NETWORK_LINK="https://www.nationalrail.co.uk/"
+
+# Date and Time of last analysis in UTC and Local Time format
+# automatically build by PHP script
+
+# Date and Time of latest changes in UTC and Local Time format
+# automatically build by PHP script
+
+# Name + Link to discussion / documentation page (usually in OSM Wiki)
+PTNA_WWW_DISCUSSION_NAME="Discussion"
+PTNA_WWW_DISCUSSION_LINK="https://wiki.openstreetmap.org/wiki/$ANALYSIS_TALK"
+
+# Name + Link to list of expected public ransport routes page (usually in OSM Wiki but can als be on GitHub)
+PTNA_WWW_ROUTES_NAME="National Rail Routes in Central Belt, Scotland"
+PTNA_WWW_ROUTES_LINK="https://wiki.openstreetmap.org/wiki/$WIKI_ROUTES_PAGE"
