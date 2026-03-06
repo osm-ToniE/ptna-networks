2a3aabb48baa73db7e0e29b2e02cff18768715d1
Toni Erdmann <Toni.Erdmann@web.de>
2026-03-06 13:57:38 +0100

    add  ore 'use extract' configurations for US time zone UTC-05

--- UTC-05/US/TN/US-TN-KAT/settings.sh
+++ UTC-05/US/TN/US-TN-KAT/settings.sh
@@ -8,6 +8,12 @@ PREFIX="US-TN-KAT"
 
 PTNA_TIMEZONE="America/New_York"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="TN"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:900];area[wikidata='Q185582'];(rel(area)[~'route'~'(bus)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Knoxville Area Transit"
 NETWORK_SHORT="KAT"
