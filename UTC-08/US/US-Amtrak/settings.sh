cc15ff4907fb528b98a45fbc168b0663ccf2797f
Toni Erdmann <Toni.Erdmann@web.de>
2026-05-03 11:11:18 +0200

    do not use OVERPASS_REUSE_ID, not shared

--- UTC-08/US/US-Amtrak/settings.sh
+++ UTC-08/US/US-Amtrak/settings.sh
@@ -14,9 +14,6 @@ PTNA_EXTRACT_SOURCE="level0-us-continental-us.osm.pbf"
 # ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
 PTNA_EXTRACT_GETIDS="north-america-filtered"
 
-# avoid downloading same area/data if the data has already been downloaded and is not older than 1 hour (start analysis with: "ptna-networks.sh -fo" to 'f'orce download)
-OVERPASS_REUSE_ID="US-Q30-train"
-
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=[timeout:1800];area[wikidata='Q30'];(rel(area)[~'route'~'(train)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="Amtrak"
 NETWORK_SHORT=""
