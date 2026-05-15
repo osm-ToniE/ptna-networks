2256d62534f161ad8fb1d1cdc30caba619a68a6f
Toni Erdmann <Toni.Erdmann@web.de>
2026-05-15 22:28:30 +0200

    add some more 'use extracts' configuration files

--- UTC+07/ID/JK/ID-JK-Transjakarta/settings.sh
+++ UTC+07/ID/JK/ID-JK-Transjakarta/settings.sh
@@ -12,7 +12,7 @@ PTNA_TIMEZONE="Asia/Jakarta"
 PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
 # Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
 # ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
-PTNA_EXTRACT_GETIDS="level3-id-state-java"
+PTNA_EXTRACT_GETIDS="level3-ID-state-java"
 
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata~'^(Q3630|Q3540|Q3724)$'][type=boundary];(rel(area)[~'route'~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG="PT Transportasi Jakarta"
