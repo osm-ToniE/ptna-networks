a2140c6a01e5b4e2dd1cfdf1f3a75dead6ff54a0
Toni Erdmann <Toni.Erdmann@web.de>
2026-05-17 14:56:17 +0200

    add some more 'use extract' configuration settings and files

--- UTC+05.30/IN/MH/IN-MH-Mumbai/settings.sh
+++ UTC+05.30/IN/MH/IN-MH-Mumbai/settings.sh
@@ -8,6 +8,12 @@ PREFIX="IN-MH-Mumbai"
 
 PTNA_TIMEZONE="Asia/Kolkata"
 
+# PTNA extract source is an alternative to using Overpass API: use planet extracts. This file has been filtered for relevant data (similar to '[~'route'~'(bus|tram|train|...') during planet handling
+PTNA_EXTRACT_SOURCE="$PREFIX.osm.pbf"
+# Relations as members of route_master/route relations cannot be extracted ("osmium extract") if they are outside the search area.
+# ptna-routes.pl will report their IDs to STDERR (*.log), they can be retrieved from a larger file using "osmium getid"
+PTNA_EXTRACT_GETIDS="level4-IN-state-MH"
+
 OVERPASS_QUERY="https://overpass-api.de/api/interpreter?data=area[wikidata='Q3621720'][type=boundary];(rel(area)[~'route'~'(coach|bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)'];rel(br);rel[type=%27route%27](r);)-%3E.routes;(.routes;%3C%3C;rel(r.routes);way(r);node(w);way(r.routes);node(w);node(r.routes););out;"
 NETWORK_LONG=""
 NETWORK_SHORT=""
