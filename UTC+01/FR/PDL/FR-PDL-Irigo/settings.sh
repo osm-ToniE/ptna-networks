2958f95aa9fa88d8bf423694cdc02847a3a9cbc6
ToniE <osm-ToniE@web.de>
2026-09-13 16:21:25 +0200

    Update settings.sh, switch off check-platform

--- UTC+01/FR/PDL/FR-PDL-Irigo/settings.sh
+++ UTC+01/FR/PDL/FR-PDL-Irigo/settings.sh
@@ -22,7 +22,7 @@ ANALYSIS_PAGE="Pays_de_la_Loire/Transports_en_commun/Analyse"
 ANALYSIS_TALK="Talk:Pays_de_la_Loire/Transports_en_commun/Analyse"
 WIKI_ROUTES_PAGE="Pays_de_la_Loire/Transports_en_commun/Irigo/PTNA_csv"
 
-ANALYSIS_OPTIONS="--timezone=$PTNA_TIMEZONE --language=fr --check-bus-stop --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-platform --check-roundabouts --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
+ANALYSIS_OPTIONS="--timezone=$PTNA_TIMEZONE --language=fr --check-bus-stop --check-gtfs --link-gtfs --show-gtfs --gtfs-feed=$PREFIX --max-error=10 --check-access --check-dates --check-way-type --check-service-type --check-name-relaxed --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --check-roundabouts --multiple-ref-type-entries=analyze --coloured-sketchline --relaxed-begin-end-for=train,subway,light_rail,monorail,tram"
 
 # --expect-network-short
 # --expect-network-short-for=
